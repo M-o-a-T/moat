b66fe52640088bf31bcff4def678bbbaf626587e
Matthias Urlichs <matthias@urlichs.de>
2019-10-13 19:48:22 +0200

    Add basic code for a fake bus, for testing

---
+++ fakebus/bus.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+
+"""
+This is a simulated MoaT bus. It offers a Unix socket. Connected
+"""
+import anyio
+import asyncclick as click
+from contextlib import asynccontextmanager
+import time
+import os
+from random import random
+
+_seq = 0
+
+class Main:
+    def __init__(self, **kw):
+        self.clients = set()
+        self.trigger = anyio.create_event()
+        for k,v in kw.items():
+            setattr(self,k,v)
+        self.t = None
+
+    async def trigger_update(self):
+        await self.trigger.set()
+
+    async def add(self, client):
+        self.clients.add(client)
+        await self.trigger_update()
+
+    async def remove(self, client):
+        self.clients.remove(client)
+        await self.trigger_update()
+        if not self.clients:
+            self.t = None
+
+    def report(self, n, val):
+        if not self.verbose:
+            return
+        t = time.monotonic()
+        self.t,t = t,(0 if self.t is None else t-self.t)
+
+        n = "%02d"%n if n else "--"
+
+        print("%6.3f %s %02x" % (t,n,val))
+
+    async def run(self):
+        last = -1
+        while True:
+            await self.trigger.wait()
+            self.trigger = anyio.create_event()
+
+            val = 0xFF
+            for c in self.clients:
+                val &= c.data
+
+            await anyio.sleep((random()*self.max_delay+self.delay)/1000)
+            if last != val:
+                self.report(0,val)
+                last = val
+
+            b = bytes((val,))
+            for c in self.clients:
+                await c.send_all(b)
+
+@asynccontextmanager
+async def mainloop(tg,**kw):
+    mc = Main(**kw)
+    await tg.spawn(mc.run)
+    yield mc
+
+async def serve(client, loop):
+    global _seq
+    _seq += 1
+    n = _seq
+
+    client.data = 0xFF
+    await loop.add(client)
+    await loop.trigger_update()
+    try:
+        async with client:
+            while True:
+                data = await client.receive_some(1)
+                if not data:
+                    return
+                client.data = data[0]
+                if loop.verbose:
+                    loop.report(n,client.data)
+                await loop.trigger_update()
+    finally:
+        await loop.remove(client)
+
+@click.command()
+@click.option("-s","--socket", help="Socket to use",default="/tmp/moatbus")
+@click.option("-v","--verbose", help="Report changes",is_flag=True)
+@click.option("-d","--delay", type=int,help="fixed delay (msec)",default=0)
+@click.option("-D","--max-delay", type=int,help="random delay (msec)",default=0)
+async def main(socket, **kw):
+    try:
+        os.unlink(socket)
+    except EnvironmentError:
+        pass
+    async with anyio.create_task_group() as tg, \
+            await anyio.create_unix_server(socket) as server, \
+            mainloop(tg, **kw) as loop:
+        evt = anyio.create_event()
+        async for client in server.accept_connections():
+            await tg.spawn(serve, client, loop)
+
+if __name__ == "__main__":
+    main()
+
---
+++ fakebus/raw_bus.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <poll.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, const char *argv[])
+{
+    struct sockaddr_un address;
+    int  socket_fd, nbytes;
+    char buffer[256];
+
+    socket_fd = socket(PF_UNIX, SOCK_STREAM, 0);
+    if(socket_fd < 0)
+    {
+		printf("socket() failed\n");
+		return 1;
+    }
+
+    /* start with a clean address structure */
+    memset(&address, 0, sizeof(struct sockaddr_un));
+
+    address.sun_family = AF_UNIX;
+    strcpy(address.sun_path, (argc>1) ? argv[1] : "/tmp/moatbus");
+
+    if(connect(socket_fd, 
+               (struct sockaddr *) &address, 
+               sizeof(struct sockaddr_un)) != 0)
+    {
+		printf("connect() failed\n");
+		return 1;
+    }
+
+	while(1) {
+		unsigned char c;
+		struct pollfd p = {socket_fd,POLLIN};
+		switch(poll(&p,1,1000)) {
+		case -1:
+			perror("poll");
+    		close(socket_fd);
+			return 1;
+		case 0:
+			c = 0xFF & ~(1<<(rand()%8));
+    		write(socket_fd, &c, 1);
+			break;
+		case 1:
+    		switch(read(socket_fd, &c, 1)) {
+			case -1:
+				perror("poll");
+				close(socket_fd);
+				return 1;
+			case 0:
+				close(socket_fd);
+				return 0;
+			case 1:
+				printf("%02x ",c);
+				fflush(stdout);
+				break;
+			}
+		}
+	}
+    close(socket_fd);
+    return 0;
+}

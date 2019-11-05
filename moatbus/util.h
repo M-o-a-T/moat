dec93593b6ccc9f7af6422c6454de1e138f451c9
Matthias Urlichs <matthias@urlichs.de>
2019-11-05 11:59:30 +0100

    Nicer loop for powi

--- moatbus/util.h
+++ moatbus/util.h
@@ -1,12 +1,20 @@
 #ifndef MOATBUS_UTIL
 #define MOATBUS_UTIL
 
+// fast small-integer powers
 static inline u_int16_t powi(u_int8_t x, u_int8_t y)
 {
     u_int16_t res = 1;
-    while (y-- > 0)
-        res *= x;
-    return res;
+    u_int16_t xx = x;
+
+    while(1) {
+        if (y&1)
+            res *= xx;
+        y >>= 1;
+        if (!y)
+            return res;
+        xx *= xx;
+    }
 }
 
 #endif

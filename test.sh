c5c74ac14c4d89f5a178e759ae338c4715a9eff9
Matthias Urlichs <matthias@urlichs.de>
2020-05-24 21:10:06 +0200

    Add test script.
    TODO: do the actual tests, beyond setting it all up.

---
+++ test.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -uxe
+chip="gpio-mockup-A"
+cur="$(pwd)"
+
+rmmod gpio-mockup >/dev/null 2>&1 || true
+if lsmod | grep -sq gpio-mockup  ; then
+	echo "Could not remove the gpio-mockup module. Exiting." >&2
+	exit 1
+fi
+modprobe gpio-mockup gpio_mockup_ranges=-1,8
+cd /sys/class/gpio/
+for d in gpiochip* ; do
+	if test "$(cat $d/label)" = "$chip" ; then
+		D=$d
+		break
+	fi
+done
+E="/sys/kernel/debug/gpio-mockup-event/$chip"
+H="$(hostname | sed -e 's/\..*//')"
+cd "$cur"
+
+export PYTHONPATH=.:../asyncgpio
+
+distkv client gpio port -t input -m read -a dest "test a b gpio one" $H $chip 1
+distkv client gpio port -t input -m count -a dest "test a b gpio two" $H $chip 2
+
+distkv client data set -ev False test a b gpio three
+distkv client data set -ev False test a b gpio four
+distkv client data set -ev False test a b gpio five
+
+distkv client gpio port -t output -m write -a src "test a b gpio three" -a state "test a b gpio three state" $H $chip 3
+distkv client gpio port -t output -m oneshot -a src "test a b gpio four" -a state "test a b gpio four state" -a t_on 3 $H $chip 4
+distkv client gpio port -t output -m pulse -a src "test a b gpio five" -a state "test a b gpio five state" -a t_on 3 -a t_off 5 $H $chip 5
+
+cat <<END
+All set up. Now, from a different terminal, play with setting "test a b gpio three" through "five"
+and/or "echo -n 1 >/sys/kernel/debug/gpio-mockup-event/gpiochip3/1" or 2.
+
+You can observe GPIO states with "cat /sys/kernel/debug/gpio".
+
+TODO: automate that. As kernels on all those test VMs out there don't compile
+the gpio-mockup module, I didn't do that yet. Feel free.
+END
+
+distkv -vvvv client gpio monitor $H $chip
+
+

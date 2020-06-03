6cec1058ef4e36f468970377efa21b23161fc318
Matthias Urlichs <matthias@urlichs.de>
2020-06-03 08:13:19 +0200

    Add code to take down tasks properly when updating
    TODO: removing pins isn't covered

--- distkv_ext/gpio/model.py
+++ distkv_ext/gpio/model.py
@@ -87,6 +87,8 @@ class GPIOline(_GPIOnode):
     """Describes one GPIO line.
     """
     _work = None
+    _task_scope = None
+    _task_done = None
 
     def __init__(self,*a,**k):
         super().__init__(*a,**k)
@@ -110,6 +112,19 @@ class GPIOline(_GPIOnode):
         else:
             await self.root.err.record_error("gpio", *self.subpath, comment="Line type not set", data={"path":self.subpath,"typ":typ})
 
+    async def _task(self,p,*a,**k):
+        if self._task_scope is not None:
+            await self._task_scope.cancel()
+            await self._task_done.wait()
+        try:
+            async with anyio.open_cancel_scope() as sc:
+                self._task_scope = sc
+                self._task_done = anyio.create_event()
+                await p(*a,**k)
+        finally:
+            self._task_scope = None
+            await self._task_done.set()
+
     # Input #
 
     async def _poll_task(self, evt, dest):
@@ -129,7 +144,7 @@ class GPIOline(_GPIOnode):
         skip = self.find_cfg('skip')
         bounce = self.find_cfg('t_bounce')
         idle = self.find_cfg('t_idle')
-        idle_h = self.find_cfg('t_idle_on',idle)
+        idle_h = self.find_cfg('t_idle_on', default=idle)
         idle_clear = self.find_cfg('t_clear')
         count = self.find_cfg('count')
         flow = self.find_cfg('flow')
@@ -346,13 +361,13 @@ class GPIOline(_GPIOnode):
 
         evt = anyio.create_event()
         if mode == "read":
-            await self.task_group.spawn(self._poll_task, evt, dest)
+            await self.task_group.spawn(self._task,self._poll_task, evt, dest)
         elif mode == "count":
             # These two are in the global config and thus can't raise KeyError
-            await self.task_group.spawn(self._count_task, evt, dest)
+            await self.task_group.spawn(self._task,self._count_task, evt, dest)
         elif mode == "button":
             # These two are in the global config and thus can't raise KeyError
-            await self.task_group.spawn(self._button_task, evt, dest)
+            await self.task_group.spawn(self._task,self._button_task, evt, dest)
         else:
             await self.root.err.record_error("gpio", *self.subpath, comment="mode unknown", data={"path":self.subpath, "mode":mode})
             return
@@ -527,12 +542,12 @@ class GPIOline(_GPIOnode):
 
         evt = anyio.create_event()
         if mode == "write":
-            await self.task_group.spawn(self.with_output, evt, src, self._set_value, state, negate)
+            await self.task_group.spawn(self._task, self.with_output, evt, src, self._set_value, state, negate)
         elif mode == "oneshot":
             if t_on is None:
                 await self.root.err.record_error("gpio", *self.subpath, comment="t_on not set", data={"path":self.subpath})
                 return
-            await self.task_group.spawn(self.with_output, evt, src, self._oneshot_value, state, negate, t_on)
+            await self.task_group.spawn(self._task, self.with_output, evt, src, self._oneshot_value, state, negate, t_on)
         elif mode == "pulse":
             if t_on is None:
                 await self.root.err.record_error("gpio", *self.subpath, comment="t_on not set", data={"path":self.subpath})
@@ -540,7 +555,7 @@ class GPIOline(_GPIOnode):
             if t_off is None:
                 await self.root.err.record_error("gpio", *self.subpath, comment="t_off not set", data={"path":self.subpath})
                 return
-            await self.task_group.spawn(self.with_output, evt, src, self._pulse_value, state, negate, t_on, t_off)
+            await self.task_group.spawn(self._task, self.with_output, evt, src, self._pulse_value, state, negate, t_on, t_off)
         else:
             await self.root.err.record_error("gpio", *self.subpath, comment="mode unknown", data={"path":self.subpath, "mode":mode})
             return

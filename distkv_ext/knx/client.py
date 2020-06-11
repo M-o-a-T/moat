3bea8feca9ca1977475995464bd8c7ffde3805cf
Matthias Urlichs <matthias@urlichs.de>
2020-06-11 16:21:13 +0200

    Omnipatch to get input to work
    Output untested

--- distkv_ext/knx/client.py
+++ distkv_ext/knx/client.py
@@ -42,10 +42,10 @@ async def dump(obj, path):
     yprint(res, stream=obj.stdout)
 
 
-@cli.command()
+@cli.command(name="list")
 @click.argument("path", nargs=-1)
 @click.pass_obj
-async def list(obj, path):
+async def list_(obj, path):
     """List the next stage.
     """
     res = {}
@@ -79,53 +79,37 @@ async def attr_(obj, attr, value, path, eval_, split):
     await _attr(obj, attr, value, path, eval_)
 
 @cli.command()
-@click.option("-m", "--mode", help="Port mode. Use '-' to disable.")
+@click.option("-t", "--type", "typ", help="Must be 'in' or 'out'.")
+@click.option("-m", "--mode", help="Use '-' to disable.")
 @click.option("-a", "--attr", nargs=2, multiple=True, help="One attribute to set (NAME VALUE). May be used multiple times.")
 @click.argument("path", nargs=-1)
 @click.pass_obj
-async def port(obj, path, mode, attr):
+async def addr(obj, path, typ, mode, attr):
     """Set/get/delete port settings. This is a shortcut for the "attr" command.
 
     \b
-    Known attributes for modes:
-      input:
-        read: dest (path)
-        count: + interval (float), count (+-x for up/down/both)
-      output:
-        write: src (path), state (path)
-        oneshot: + t_on (float), rest (+-), state (path)
-        pulse:   + t_off (float)
+    Known attributes:
+      type=in:
+        mode (data type)
+        dest (path)
+      type=out:
+        mode (data type)
+        src (path)
 
     \b
     Paths elements are separated by spaces.
-    "rest" is the state of the wire when the input is False.
-    Floats may be paths, in which case they're read from there when starting.
     """
     if len(path) != 4:
         raise click.UsageError("Path must be 4 elements: bus+ga1+ga2+ga3.")
     res = await obj.client.get(*obj.cfg.knx.prefix, *path_eval(path, (2,3,4)), nchain=obj.meta or 1)
     val = res.get('value', attrdict())
 
+    if typ:
+        attr = (('type', typ),) + attr
     if mode:
         attr = (('mode', mode),) + attr
     for k,v in attr:
-        if k == "count":
-            if v == '+':
-                v = True
-            elif v == '-':
-                v = False
-            elif v in 'xX*':
-                v = None
-            else:
-                v = click.UsageError("'count' wants one of + - X")
-        elif k == "rest":
-            if v == '+':
-                v = True
-            elif v == '-':
-                v = False
-            else:
-                v = click.UsageError("'rest' wants one of + -")
-        elif k in {"src", "dest"} or ' ' in v:
+        if k in {"src", "dest"} or ' ' in v:
             v = v.split(' ')
             v = tuple(x for x in v if x != '')
         else:
@@ -136,17 +120,16 @@ async def port(obj, path, mode, attr):
                     v = float(v)
                 except ValueError:
                     pass
-        if isinstance(v,click.UsageError):
-            raise v
         val[k] = v
 
     await _attr(obj, (), val, path, False, res)
 
-async def _attr(obj, attr, value, path, eval_, res=None):
+async def _attr(obj, attr, value, path, eval_, res=None, server=False):
     # Sub-attr setter.
     # Special: if eval_ is True, an empty value deletes. A mapping replaces instead of updating.
     if res is None:
-        res = await obj.client.get(*obj.cfg.knx.prefix, *path_eval(path, (2,3,4)), nchain=obj.meta or (value is not None))
+        res = await obj.client.get(*obj.cfg.knx.prefix, *(path if server
+            else path_eval(path, (2,3,4))), nchain=obj.meta or (value is not None))
     try:
         val = res.value
     except AttributeError:
@@ -172,7 +155,7 @@ async def _attr(obj, attr, value, path, eval_, res=None):
             return
         value = res_update(res, *attr, value=value)
 
-    res = await obj.client.set(*obj.cfg.knx.prefix, *path_eval(path, (2,3,4)), value=value, nchain=obj.meta, chain=res.chain)
+    res = await obj.client.set(*obj.cfg.knx.prefix, *(path if server else path_eval(path, (2,3,4))), value=value, nchain=obj.meta, chain=res.chain)
     if obj.meta:
         yprint(res, stream=obj.stdout)
 
@@ -181,23 +164,23 @@ async def _attr(obj, attr, value, path, eval_, res=None):
 @click.option("-h","--host",help="Host name of this server.")
 @click.option("-p","--port",help="Port of this server.")
 @click.option("-d","--delete",is_flag=True, help="Delete this server.")
+@click.argument("bus", nargs=1)
 @click.argument("name", nargs=-1)
 @click.pass_obj
-async def server(obj, name, host, port, delete):
+async def server(obj, bus, name, host, port, delete):
     """
-    Configure a server.
-
-    No arguments: list them.
+    Configure a server for a bus.
     """
     if not name:
         if host or port or delete:
             raise click.UsageError("Use a server name to set parameters")
-        async for r in obj.client.get_tree(*obj.cfg.knx.prefix, min_depth=1, max_depth=1):
+        async for r in obj.client.get_tree(*obj.cfg.knx.prefix, bus, min_depth=1, max_depth=1):
             print(r.path[-1], file=obj.stdout)
         return
     elif len(name) > 1:
         raise click.UsageError("Only one server allowed")
     name = name[0]
+
     if host or port:
         if delete:
             raise click.UsageError("You can't delete and set at the same time.")
@@ -216,20 +199,22 @@ async def server(obj, name, host, port, delete):
         return
     else:
         value = None
-    await _attr(obj, ("server",), value, (name,), False)
+    await _attr(obj, (), value, (bus,name), False, server=True)
 
 
 @cli.command()
-@click.argument("name", nargs=1)
+@click.option("-l","--local-ip",help="Force this local IP address.")
+@click.argument("bus", nargs=1)
+@click.argument("server", nargs=1)
 @click.pass_obj
-async def monitor(obj, name):
-    """Stand-alone task to monitor a single contoller.
+async def monitor(obj, bus, server, local_ip):
+    """Stand-alone task to talk to a single server.
     """
     from distkv_ext.knx.task import task
     from distkv_ext.knx.model import KNXroot
-    server = await KNXroot.as_handler(obj.client)
-    await server.wait_loaded()
+    knx = await KNXroot.as_handler(obj.client)
+    await knx.wait_loaded()
 
     async with as_service(obj) as srv:
-        await task(obj.client, obj.cfg.knx, server[name], srv)
+        await task(obj.client, obj.cfg.knx, knx[bus][server], srv, local_ip=local_ip)
 

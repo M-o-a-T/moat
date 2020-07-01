c289ff47d706c4a64983498b920513151bdaa5e4
Matthias Urlichs <matthias@urlichs.de>
2020-07-01 10:42:14 +0200

    Stupid

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+from setuptools import setup
+
 LONG_DESC = open("README.rst").read()
 
-setup(  # pylint: disable=undefined-variable
+setup(
     name="distwago",
     use_scm_version={"version_scheme": "guess-next-dev", "local_scheme": "dirty-tag"},
     description="A distributed no-master key-value store",

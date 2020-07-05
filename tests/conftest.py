e1fe33b1ddadd09a46c9ae70b66615f3731a695d
Matthias Urlichs <matthias@urlichs.de>
2020-07-05 12:09:07 +0200

    Fix pylint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from pytest_trio.enable_trio_mode import *  # noqa: F403,F401
+from pytest_trio.enable_trio_mode import *  # pylint:disable=wildcard-import,unused-wildcard-import
 
 import logging
 

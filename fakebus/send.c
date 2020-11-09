89dd4e48e29be1528e0b5a9f0abd41154067e60c
Matthias Urlichs <matthias@urlichs.de>
2020-11-09 15:19:09 +0100

    fake_send: multiple messages

--- fakebus/send.c
+++ fakebus/send.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <getopt.h>
+#include <time.h>
 
 #include "fakebus/client.h"
 
@@ -16,13 +17,15 @@ static struct option long_options[] = {
     {"source", required_argument,  0, 'S'},
     {"dest",   required_argument,  0, 'D'},
     {"cmd",    required_argument,  0, 'C'},
+    {"n_msg",  required_argument,  0, 'n'},
+    {"n_delay",required_argument,  0, 'N'},
     {"wires",  required_argument,  0, 'b'},
     {"verbose",no_argument,        0, 'v'},
     {0,         0,                 0,  0 }
 };
 
 /*
- * A short test program to send random wire states to the fake bus
+ * A short test program to send a message to the fake bus
  */
 int main(int argc, char* const* argv)
 {
@@ -36,13 +39,16 @@ int main(int argc, char* const* argv)
     int src = 1;
     int dest = 2;
     int cmd = 0;
+    int n_msg = 1;
+    int n_delay = 5;
+    char has_fc_timer = 0;
     char verbose = 0;
     int n_wires = 3;
 
     FakeClient fc;
     int opt;
 
-    while((opt = getopt(argc, argv, "b:C:D:s:S:t:T:v")) != -1) {
+    while((opt = getopt(argc, argv, "b:C:D:n:N:s:S:t:T:v")) != -1) {
         switch (opt) {
         case 's':
             sockname = optarg;
@@ -55,6 +61,12 @@ int main(int argc, char* const* argv)
             break;
         case 'D':
             dest = atoi(optarg);
+            break;
+        case 'n':
+            n_msg = atoi(optarg);
+            break;
+        case 'N':
+            n_delay = atof(optarg);
             break;
         case 'S':
             src = atoi(optarg);
@@ -83,10 +95,11 @@ int main(int argc, char* const* argv)
     if(!fc_connect(fc, sockname))
         return 1;
 
-    {
-        const char *arg = argv[optind];
-        if (arg == NULL)
-            arg = "!";
+    const char *arg = argv[optind];
+    if (arg == NULL)
+        arg = "!";
+
+    void msg_send() {
         BusMessage msg = msg_alloc(strlen(arg));
         msg->src = src;
         msg->dst = dest;
@@ -96,16 +109,35 @@ int main(int argc, char* const* argv)
         fc_send(fc, msg);
     }
 
+    time_t t1 = time(NULL)-n_delay;
+
+    int timeout() {
+        int t = time(NULL)-t1;
+        int f = fc_timeout(fc);
+        has_fc_timer=(f>0);
+        if (t>n_delay)
+            return 0;
+        t = (n_delay-t)*1000;
+        if (f < 0)
+            return t;
+        return (f<t)?f:t;
+    }
+
     while(1) {
         unsigned char c;
         struct pollfd p = {fc->socket_fd,POLLIN,0};
-        switch(poll(&p,1,fc_timeout(fc))) {
+        switch(poll(&p,1,timeout())) {
         case -1:
             perror("poll");
             fc_free(fc);
             return 1;
         case 0:
-            fc_timer(fc);
+            if(time(NULL)-t1 > n_delay) {
+                t1=time(NULL);
+                msg_send();
+            }
+            if(has_fc_timer)
+                fc_timer(fc);
             break;
         case 1:
             fc_process(fc);
@@ -113,15 +145,16 @@ int main(int argc, char* const* argv)
         }
 
         if(fc->in_msg) {
-            printf("GotMsg\n");
+            printf("Got %s\n",msg_info(fc->in_msg));
             msg_free(fc->in_msg);
             fc->in_msg = NULL;
         }
         if(fc->out_msg) {
-            printf("SentMsg %d\n",fc->out_result);
+            printf("Sent %s %d\n",arg,fc->out_result);
             msg_free(fc->out_msg);
             fc->out_msg = NULL;
-            break;
+            if(!--n_msg)
+                break;
         }
     }
     fc_free(fc);

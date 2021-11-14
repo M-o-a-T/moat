9a77ef684f1798cf6c60b663ebdf111e4a35062a
Matthias Urlichs <matthias@urlichs.de>
2021-11-14 14:51:02 +0100

    owch

--- moatbus/handler.c
+++ moatbus/handler.c
@@ -887,8 +887,9 @@ static void h_set_state(Handler h, enum _S state)
 
     if(DEBUG_WIRE ||
         ((state >= S_READ) != (h->state >= S_READ)) ||
-        ((state >= S_WRITE) != (h->state >= S_WRITE)) )
+        ((state >= S_WRITE) != (h->state >= S_WRITE)) ) {
         // h_debug(h, "SetState %s",h_state_name(state));
+    }
 
 
     if((state < S_WRITE) && (h->state >= S_WRITE)) {

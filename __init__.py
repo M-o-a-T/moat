39a8e6dfbf5e465163b5c2677eb1c2287db78ef3
Matthias Urlichs <matthias@urlichs.de>
2022-07-05 12:46:50 +0000

    raise errors instead of suppressing them

--- __init__.py
+++ __init__.py
@@ -6,59 +6,59 @@ from ._dict import *  # noqa: F401,F403  # isort:skip
 try:
     from ._event import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._ctx import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._queue import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._msgpack import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._module import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._msg import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._path import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._server import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._spawn import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._systemd import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._yaml import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise
 
 try:
     from ._main import *  # noqa: F401,F403
 except ImportError:
-    pass
+    raise

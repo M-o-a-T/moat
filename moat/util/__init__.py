693a0032780571d807dcef808f6e62c0d52c7d01
Matthias Urlichs <matthias@urlichs.de>
2022-09-06 10:14:22 +0200

    Log import failures

--- moat/util/__init__.py
+++ moat/util/__init__.py
@@ -1,65 +1,68 @@
 # TODO split this up
 
+import logging as _logging
+_log = _logging.getLogger(__name__)
+
 from ._impl import *  # noqa: F401,F403  # isort:skip
 from ._dict import *  # noqa: F401,F403  # isort:skip
 from ._merge import *  # noqa: F401,F403  # isort:skip
 
 try:
     from ._event import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._ctx import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._queue import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._msgpack import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._module import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._msg import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._path import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._server import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._spawn import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._systemd import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._yaml import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)
 
 try:
     from ._main import *  # noqa: F401,F403
-except ImportError:  # pylint: disable=try-except-raise
-    raise
+except ImportError as exc:
+    _log.warning("Missing: %s", exc)

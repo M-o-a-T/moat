880f9f02d99d713aca79c50ade2ffb6be76fd3b4
Matthias Urlichs <matthias@urlichs.de>
2022-10-04 10:37:31 +0200

    ignore nonsense

---
+++ moat/bms/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic tool support
+
+"""
+from getopt import getopt
+
+import asyncclick as click
+
+import logging  # pylint: disable=wrong-import-position
+
+log = logging.getLogger()
+
+
+@click.group()
+async def main():
+    """Battery Manager"""
+    pass  # pylint: disable=unnecessary-pass
+

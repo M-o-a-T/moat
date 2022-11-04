35e5522f2f11de5b8a63891dfdc7e13152d7ce0d
Matthias Urlichs <matthias@urlichs.de>
2022-11-04 17:44:12 +0100

    split poll and poll1 command
    the latter takes one file for a single host

--- moat/modbus/dev/_main.py
+++ moat/modbus/dev/_main.py
@@ -1,12 +1,15 @@
 import asyncclick as click
 
-from moat.util import yload, yprint, attrdict
+from moat.util import yload, yprint, attrdict, merge, to_attrdict
 import anyio
 from .device import fixup,Device
 from .poll import poll
 from ..client import ModbusClient
 from contextlib import AsyncExitStack
+from functools import partial
 
+import logging
+logger = logging.getLogger(__name__)
 
 @click.group()
 def cli():
@@ -28,7 +31,7 @@ def dump(path):
 @click.argument("path", nargs=1, type=click.File("r"))
 @click.argument("slot", nargs=-1)
 @click.pass_context
-async def poll(ctx, host, port, unit, path, slot):
+async def poll1(ctx, host, port, unit, path, slot):
     """Poll a Modbus device and forward"""
     obj = ctx.obj
 
@@ -56,5 +59,61 @@ async def poll(ctx, host, port, unit, path, slot):
     await dev.poll(set(slot))
 
     
+@cli.command()
+@click.argument("path", type=click.File("r"))
+@click.pass_context
+async def poll(ctx, path):
+    """Poll Modbus devices and forward. Hosts/Units/."""
+    obj = ctx.obj
+
+    d = yload(path, attr=True)
+    d = fixup(d)
+
+    s = d.setdefault("src", attrdict())
+    sl = d.setdefault("slots", attrdict())
+
+    if "distkv" in obj.cfg:
+        from distkv.client import open_client
+        from .distkv import Register
+        from functools import partial
+        dkv = await ctx.with_async_resource(open_client(**obj.cfg.distkv))
+        #tg = await ctx.with_async_resource(anyio.create_task_group())
+        #Reg = partial(Register, dkv=dkv, tg=tg)
+    else:
+        from .device import Register as Reg
+
+    async with ModbusClient() as cl, anyio.create_task_group() as tg:
+        nd = 0
+        async def poll(v, **kw):
+            kw = to_attrdict(kw)
+            vs = v.setdefault("src", attrdict())
+            merge(vs,kw,s, replace=False)
+            vsl = v.setdefault("slots", attrdict())
+            merge(vsl,sl, replace=False)
+
+            logger.info("Starting %r", vs)
+            dev = Device(client=cl, factory=Reg)
+            dev.load(data=v)
+            await dev.poll()
+
+        async with anyio.create_task_group() as tg:
+            for h,hv in d.get("hosts",{}).items():
+                for u,v in hv.items():
+                    Reg = partial(Register, dkv=dkv, tg=tg)
+                    tg.start_soon(partial(poll, v, host=h, unit=u))
+                    nd += 1
+        
+            for h,hv in d.get("hostports",{}).items():
+                for p,pv in hv.items():
+                    if not isinstance(p,int):
+                        continue
+                    for u,v in pv.items():
+                        tg.start_soon(partial(poll(v, host=h, port=p, unit=u)))
+                        nd += 1
+
+        if not nd:
+            logger.error("No devices to poll found.")
+
+        
 
 

527e1d3a9162034db5435387bfaad3995b48d321
morph027 <stefan.heitmueller@gmx.com>
2022-11-08 20:30:35 +0100

    add attachments
    Signed-off-by: morph027 <stefan.heitmueller@gmx.com>

--- pysignalclijsonrpc/api.py
+++ pysignalclijsonrpc/api.py
@@ -2,8 +2,13 @@
 pysignalclijsonrpc.api
 """
 
+from base64 import b64decode
+from io import BytesIO
+from mimetypes import guess_extension
+from tempfile import mkstemp
 from uuid import uuid4
 
+from magic import from_buffer
 from requests import Session
 
 
@@ -18,19 +23,23 @@ class SignalCliRestApi:
     SignalCliRestApi
     """
 
-    def __init__(self, endpoint: str, account: str, verify_ssl: bool = True):
+    def __init__(
+        self, endpoint: str, account: str, auth: tuple = (), verify_ssl: bool = True
+    ):
         """
         SignalCliRestApi
 
         Args:
             endpoint (str): signal-cli JSON-RPC endpoint.
             account (str): signal-cli account to use.
+            auth (tuple): basic authentication credentials (e.g. `("user", "pass")`)
             verify_ssl (bool): SSL verfification for https endpoints
                 (defaults to True).
         """
         self._session = Session()
         self._endpoint = endpoint
         self._account = account
+        self._auth = auth
         self._verify_ssl = verify_ssl
 
     def _jsonrpc(self, method: str, params: object = None):
@@ -46,7 +55,10 @@ class SignalCliRestApi:
         }
         try:
             res = self._session.post(
-                url=f"{self._endpoint}", json=data, verify=self._verify_ssl
+                url=f"{self._endpoint}",
+                json=data,
+                auth=self._auth,
+                verify=self._verify_ssl,
             )
             res.raise_for_status()
             ret = res.json()
@@ -74,13 +86,21 @@ class SignalCliRestApi:
         """
         return self._jsonrpc(method="version").get("version")
 
-    def send_message(self, message: str, recipients: list):
+    def send_message(
+        self,
+        message: str,
+        recipients: list,
+        filenames: list = None,
+        attachments_as_bytes: list = None,
+    ):
         """
         Send message.
 
         Args:
             message (str): Message to be sent.
             recipients (list): List of recipients.
+            filenames (list): List of `str` w/ filenames to send as attachment(s).
+            attachments_as_bytes (list): List of `bytearray` to send as attachment(s).
 
         Returns:
             timestamp (int): The message timestamp.
@@ -88,6 +108,27 @@ class SignalCliRestApi:
         Raises:
             :exception:`pysignalclijsonrpc.api.SignalCliRestApiError`
         """
+        attachments = []
+        if filenames is not None:
+            attachments.extend(filenames)
+        if attachments_as_bytes is not None:
+            for attachment in attachments_as_bytes:
+                try:
+                    uuid = str(uuid4())
+                    attachment_io_bytes = BytesIO()
+                    attachment_io_bytes.write(b64decode(attachment))
+                    extension = guess_extension(
+                        from_buffer(attachment_io_bytes.getvalue(), mime=True)
+                    )
+                    _, filename = mkstemp(prefix=f"{uuid}_", suffix=f".{extension}")
+                    with open(filename, "wb") as f_h:
+                        f_h.write(attachment)
+                    attachments.append(filename)
+                except Exception as err:  # pylint: disable=broad-except
+                    error = getattr(err, "message", repr(err))
+                    raise SignalCliRestApiError(
+                        f"signal-cli JSON RPC request failed: {error}"
+                    ) from err
         return self._jsonrpc(
             method="send",
             params={

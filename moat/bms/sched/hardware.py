3b2ce4ac747ce1fd4fd547ee85f8555d04e3a47b
Matthias Urlichs <matthias@urlichs.de>
2022-11-17 16:11:16 +0100

    use kW/kWh

--- moat/bms/sched/hardware.py
+++ moat/bms/sched/hardware.py
@@ -7,15 +7,15 @@ from dataclasses import dataclass
 class Hardware:
     """Stores information about the hardware used.
 
-    capacity: useable battery capacity in Wh
+    capacity: useable battery capacity in kWh
     batt_max_soc: max battery SoC, default 0.95
     batt_min_soc: min battery SoC, default 0.25
-    batt_max_chg: max battery charge power in W
-    batt_max_dis: max battery discharge power in W
+    batt_max_chg: max battery charge power in kW
+    batt_max_dis: max battery discharge power in kW
     batt_eff_chg: charger efficienty (0…1)
     batt_eff_dis: discharger efficiency (0…1)
-    inv_max_chg: max charger power (AC to DC) in W
-    inv_max_dis: max inverter power (DC to AC) in W
+    inv_max_chg: max charger power (AC to DC) in kW
+    inv_max_dis: max inverter power (DC to AC) in kW
     inv_eff_chg: charger efficienty (0…1)
     inv_eff_dis: inverter efficiency (0…1)
     grid_max_in: max grid input
@@ -29,9 +29,9 @@ class Hardware:
     batt_max_dis: float = 1
     batt_eff_chg: float = 0.98
     batt_eff_dis: float = 0.98
-    inv_max_chg: float = 100
-    inv_max_dis: float = 100
+    inv_max_chg: float = 2.5
+    inv_max_dis: float = 2.5
     inv_eff_chg: float = 0.9
     inv_eff_dis: float = 0.9
-    grid_max_in: float = 99999
-    grid_max_out: float = 99999
+    grid_max_in: float = 63
+    grid_max_out: float = 63

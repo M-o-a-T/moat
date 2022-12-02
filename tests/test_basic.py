e7e2e3c88e9ba4ea9bd6a9f992ce320b67d7394e
Matthias Urlichs <matthias@urlichs.de>
2022-12-02 14:27:45 +0100

    Update from MoaT template

---
+++ tests/test_basic.py
@@ -0,0 +1,11 @@
+"""
+Empty test file
+"""
+
+import moat.lib.victron  # pylint: disable=unused-import
+
+def test_nothing():
+    """
+    Empty test
+    """
+    pass  # pylint: disable=unnecessary-pass

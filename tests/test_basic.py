6d8c1ae15d05842e64788bf117aa2768d4253f38
Matthias Urlichs <matthias@urlichs.de>
2022-12-11 17:02:32 +0100

    Update from MoaT template

---
+++ tests/test_basic.py
@@ -0,0 +1,11 @@
+"""
+Empty test file
+"""
+
+import moat.heat  # pylint: disable=unused-import
+
+def test_nothing():
+    """
+    Empty test
+    """
+    pass  # pylint: disable=unnecessary-pass

b7e562518807ff87968322b4bb39b11236c0e782
Matthias Urlichs <matthias@urlichs.de>
2022-12-11 18:06:08 +0100

    initial _main

---
+++ moat/dev/heat/_main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic tool support
+
+"""
+import logging  # pylint: disable=wrong-import-position
+
+import asyncclick as click
+from moat.util import load_subgroup
+
+log = logging.getLogger()
+
+
+@load_subgroup(sub_pre="moat.dev.heat")
+@click.pass_obj
+async def cli(obj):
+    """Device Manager for heaters"""
+    obj  # pylint: disable=pointless-statement  # TODO

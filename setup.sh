bad0af733b2d417ead59e206bc1eebb8ab05ff57
Matthias Urlichs <matthias@urlichs.de>
2022-12-12 18:27:41 +0100

    init stuff

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -ex
+
+D="$(pwd)"
+cd modbus/moat/modbus/dev/_data/heating/KWB
+sh code/rebuild.sh 
+cd "$D"
+apt install eatmydata
+eatmydata apt install \
+	python3-anyio \
+	python3-anyio-serial \
+	python3-cffi \
+	python3-gitpython \
+	python3-packaging \
+	python3-pymodbus \
+	python3-tomlkit \
+	python3-trio \
+	# end
+
+

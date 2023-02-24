2cd44a3d7a70eed84732115adcee94d3163a6f5f
Matthias Urlichs <matthias@urlichs.de>
2023-02-24 15:19:47 +0100

    Add i2c subcommand

---
+++ moat/micro/_embed/lib/app/i2c.py
@@ -0,0 +1,78 @@
+import machine
+
+from moat.micro.cmd import BaseCmd
+from moat.micro.compat import TaskGroup, sleep_ms, ticks_ms, ticks_diff
+from moat.micro.proto import SilentRemoteError as FSError
+
+import uos
+import usys
+import errno
+
+class Cmd(BaseCmd):
+    _bus_cache = None  # c,d > busobj
+
+    def __init__(self, parent, name, cfg, gcfg):
+        super().__init__(parent)
+        self._bus_cache = dict()
+        self.cfg = cfg
+
+    def _bus(self, cd, drop=False):
+        # (c,d) > bus
+        c,d = cd; cd = (c,d)
+        if drop:
+            return self._bus_cache.pop(cd)
+        else:
+            return self._bus_cache[cd]
+
+    def _add_cd(self, cd, bus):
+        # busobj > busnr
+        c,d = cd; cd = (c,d)
+        assert cd not in self._bus_cache
+        self._bus_cache[cd] = bus
+
+    def _del_cd(self,cd):
+        bus = self._bus(cd, True)
+        # bus.close()
+
+    def cmd_reset(self, p=None):
+        # close all
+        for b in self._bus_cache.values():
+            pass # b.close()
+        self._bus_cache = dict()
+
+    def cmd_open(self, c,d, cx={}, dx={}, f=1000000, t=1000000, s=False):
+        cd = (c,d)
+        if cd in self._bus_cache:
+            pass # close it
+        c = machine.Pin(c, **cx)
+        d = machine.Pin(d, **cx)
+        bus = (machine.SoftI2C if soft else machine.I2C)(scl=c,sda=d,freq=f,timeout=t)
+        _add_cd(cd,bus)
+        return cd
+
+    def cmd_rd(self, cd, i, n=16):
+        # read bus
+        bus = self._bus(cd)
+        return bus.readfrom(i,n)
+        
+    def cmd_wr(self, cd, i, buf):
+        # write bus
+        bus = self._bus(cd)
+        return bus.writeto(i,buf)
+        
+    def cmd_wrrd(self, cd, i, buf, n=16):
+        # write-then-read
+        bus = self._bus(cd)
+        d = bus.writeto(i,buf,False)
+        if d < len(buf):
+            return -d
+        return bus.readfrom(i,n)
+
+    def cmd_cl(self, cd):
+        # close
+        self._del_cd(cd)
+
+    def cmd_scan(self, cd):
+        # dir
+        bus = self._bus(cd)
+        return bus.scan()

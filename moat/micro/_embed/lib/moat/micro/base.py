2eeb97d0467d18a741b0598d2d0b1bafb764bf8c
Matthias Urlichs <matthias@urlichs.de>
2023-03-01 16:12:13 +0100

    missing import

--- moat/micro/_embed/lib/moat/micro/base.py
+++ moat/micro/_embed/lib/moat/micro/base.py
@@ -2,6 +2,7 @@ import machine
 
 from .cmd import BaseCmd
 from .compat import TaskGroup, sleep_ms, ticks_ms, ticks_diff
+from moat.util import NotGiven
 from .proto import RemoteError
 from .proto.stream import drop_proxy
 

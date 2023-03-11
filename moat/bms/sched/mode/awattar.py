91ec0ee5b25cc42f3facb91171c9b201e5577328
Matthias Urlichs <matthias@urlichs.de>
2023-03-11 11:34:40 +0100

    awattar: simple extrapolation
    because 14h isn't sufficient

--- moat/bms/sched/mode/awattar.py
+++ moat/bms/sched/mode/awattar.py
@@ -27,6 +27,18 @@ class Loader(BaseLoader):
         async with asks.sessions.Session() as s:
             r = await s.get(cfg.data.awattar.url,params=dict(start=start*1000, end=end*1000))
             dd = r.json()["data"]
-            for d in dd:
-                yield d["marketprice"] / factor + offset
+            for d in dd[24:]:
+                val = d["marketprice"] / factor + offset
+                for _ in range(cfg.steps):
+                    yield val
 
+        # Somewhat primitive linear interpolation from past data
+        s = len(dd)-24
+        dv = dd[-25]["marketplace"] - dd[-1]["marketplace"]
+        for i in range(24):
+            val = (dd[-24+i]["marketplace"] - dv*(25-i)/25) / factor + offset
+            for _ in range(cfg.steps):
+                yield val
+
+        
+        

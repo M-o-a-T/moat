3dd21c36c20148fa9dd1fd5fa70bbd21ecaf6ef3
Matthias Urlichs <matthias@urlichs.de>
2023-03-16 16:18:37 +0100

    Add a loopback messaging module, for local testing.

--- tests/loopback.py
+++ tests/loopback.py
@@ -4,6 +4,7 @@ Loopback stream module
 
 import anyio
 from moat.micro.proto.stack import _Stacked
+from random import random
 
 class Loop(_Stacked):
     """
@@ -14,9 +15,12 @@ class Loop(_Stacked):
     """
     link = None
 
-    def __init__(self, qlen=0):
+    def __init__(self, qlen=0, loss=0):
+        assert 0 <= loss < 1
+
         super().__init__(None)
         self.q_wr, self.q_rd = anyio.create_memory_object_stream(qlen)
+        self.loss = loss
 
     def link(self, other):
         """Tell this loopback to read from some other loopback."""
@@ -26,12 +30,28 @@ class Loop(_Stacked):
         """Send data."""
         if self.link is None:
             raise anyio.BrokenResourceError(self)
-        await self.q_wr.send(data)
+        if random() < self.loss:
+            return
+        try:
+            await self.q_wr.send(data)
+        except (anyio.ClosedResourceError,anyio.BrokenResourceError,anyio.EndOfStream):
+            raise EOFError
 
     async def recv(self):
         if self.link is None:
             raise anyio.BrokenResourceError(self)
-        return await self.link.q_rd.receive()
+        try:
+            return await self.link.q_rd.receive()
+        except (anyio.ClosedResourceError,anyio.BrokenResourceError,anyio.EndOfStream):
+            raise EOFError
+
+    async def run(self):
+        try:
+            await super().run()
+        finally:
+            await self.aclose()
 
     async def aclose(self):
         await self.q_wr.aclose()
+        if self.link is not None:
+            await self.link.q_rd.aclose()

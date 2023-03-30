8d96d6324bd5af2e096f803800a04b188ee19367
Matthias Urlichs <matthias@urlichs.de>
2023-03-30 09:24:20 +0200

    Clean up the generic RelADC

--- moat/micro/_embed/lib/app/part/combo.py
+++ moat/micro/_embed/lib/app/part/combo.py
@@ -3,26 +3,36 @@
 import uasyncio
 
 from moat.compat import TaskGroup
+from moat.util import attrdict
 
 class RelADC: 
     """
     A generic ADC that returns one value relative to another.
     """
     def __init__(self, cfg, **kw):
+        pin = cfg.pin
+        ref = cfg.ref
+        if not isinstance(pin,dict):
+            pin = attrdict(pin=pin)
+        if not isinstance(ref,dict):
+            ref = attrdict(pin=ref)
         for k,v in cfg.items():
-            if k not in cfg.ref:
-                cfg.ref[k] = v
+            if k in (pin,ref):
+                continue
+            pin.setdefault(k,v)
+            ref.setdefault(k,v)
+        pin.setdefault("scale",1)
+        ref.setdefault("scale",1)
+        pin.setdefault("offset",0)
+        ref.setdefault("offset",0)
 
-        self.pos = load_from_cfg(cfg, **kw)
-        self.neg = load_from_cfg(cfg.ref, **kw)
+        self.pos = load_from_cfg(pin, **kw)
+        self.neg = load_from_cfg(ref, **kw)
     
     async def run(self):
         async with TaskGroup() as tg:
-            self._tg = tg
             await tg.spawn(self.pos.run)
             await tg.spawn(self.neg.run)
-            while True:
-                await uasyncio.sleep(9999)
 
     async def read(self):
         p = n = None

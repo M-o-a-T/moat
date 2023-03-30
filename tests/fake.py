60f05d5f25877b4db1c2ebd1b62c2ed67d91dbb5
Matthias Urlichs <matthias@urlichs.de>
2023-03-30 12:45:10 +0200

    Add a fake data source.

---
+++ tests/fake.py
@@ -0,0 +1,48 @@
+"""
+fake sensors
+"""
+
+import random
+from math import tanh,exp
+
+class ADC:
+    """
+    This is a "fake" ADC that walks between a given min and max value.
+
+    The min/max boundary values will never be returned.
+
+    Config parameters:
+    - min, max: range. Defaults to 0…1.
+    - step: max difference between two consecutive values.
+    - border: A hint for how long the sequence is likely to be close to
+      the min/max. Float. Default 2.
+    - seed: used to reproduce the random sequence.
+    """
+
+    def __init__(self, cmd, cfg, **kw):
+        cmd  # unused
+        self.min = cfg.min if "min" in cfg else 0
+        self.max = cfg.max if "max" in cfg else 1
+        self.border = cfg.border if "border" in cfg else 2
+        self.step = cfg.step/(self.max-self.min)/2 if "step" in cfg else 0.1
+
+        seed = cfg.seed if "seed" in cfg else random.random()
+
+        self.val = 0
+        self.bias = 0
+        self.rand = random.Random(cfg.seed if "seed" in cfg else None)
+
+    async def read(self):
+        b = self.bias + (self.rand.random()-0.5)*self.step
+        v = self.val + b
+        if v > self.border and b > 0:
+            b = 0
+        elif v < -self.border and b < 0:
+            b = 0
+
+        self.val = v
+        self.bias = b
+
+        # tanh is steeper
+        return self.min+(self.max-self.min)*(0.5+.5*tanh(v))
+        #return self.min+(self.max-self.min)/(1+exp(v))
---
+++ tests/test_fake.py
@@ -0,0 +1,22 @@
+import pytest
+
+from moat.util import attrdict
+from .fake import ADC
+
+@pytest.mark.anyio
+@pytest.mark.parametrize("seed",range(10))
+async def test_fake(seed):
+    d = ADC(None, attrdict(min=0,max=100,step=10,seed=seed))
+    md = 0
+    mdi = 0
+    v = None
+    for i in range(100):
+        vv = await d.read()
+        assert 0<vv<100
+        if v is not None:
+            vd = abs(vv-v)
+            if vd > md:
+                md = vd
+                mdi = i
+        v = vv
+    assert 1 < md < 10,(md,mdi)

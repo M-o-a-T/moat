999ade404dc3a4a1e7505094176cbc93e6b23f8a
Matthias Urlichs <matthias@urlichs.de>
2023-04-13 08:37:11 +0200

    Teach the "link" code to use RootCmd's new broadcast/forward handlers

--- moat/micro/_embed/lib/moat/micro/link.py
+++ moat/micro/_embed/lib/moat/micro/link.py
@@ -2,14 +2,14 @@
 Helper to hot-wire a command to read data from/to the other side.
 """
 
-from moat.micro.compat import sleep
+from moat.micro.compat import sleep, wait_for_ms, TimeoutError, OptCtx, Event
 
 
 class Reader:
     """
     Base class for something that reads data.
 
-    The "send" method forwards to the other side if linked to it.
+    The "send" method forwards to the other side.
     """
     _link = None
     __cmd = None
@@ -18,8 +18,10 @@ class Reader:
         self._link = cfg.get("link", None)
 
     async def run(self, cmd):
-        self.__cmd = cmd
-        pass
+        reg = cmd.base.register(self, cmd.name, self._link) if self._link is not None else None
+        with OptCtx(reg):
+            while True:
+                await sleep(9999)
 
     async def read(self):
         res = await self.read_()
@@ -34,7 +36,7 @@ class Reader:
             return
         if self.__cmd is None:
             return
-        await self.__cmd.send_nr([self.__cmd.name, self._link], msg)
+        await self.__cmd.send_nr("s",o=(self.__cmd.name, self._link), d=msg)
 
 
 class Listener(Reader):
@@ -43,32 +45,44 @@ class Listener(Reader):
 
     Reading returns the latest/next message.
     """
-    _msg = None
-    _evt = None
+    _cmd = None
+    _up = None
+    _rd = None
     #  _link = None  # BaseReader
 
-    async def __recv(self, **msg):
-        self._msg = msg
-        self._evt.set()
+    def __init__(self, cfg, **kw):
+        super().__init__(cfg, **kw)
+        self._up = Event()
 
     async def run(self, cmd):
+        self._up.set()
         if cmd is None:
             return
         if self._link is None:
             return
+        self._rd = aiter(cmd.base.watch(cmd.name, self._link))
+        self._cmd = cmd
         try:
-            setattr(cmd, "cmd_"+self._link, self.__recv)
             while True:
-                sleep(9999)
+                await sleep(9999)
         finally:
-            if getattr(cmd, "cmd_"+self._link, None) == self.__recv:  # no, "is" doesn't work here
-                delattr(cmd, "cmd_"+self._link)
+            self._rd.close()
 
-    async def read(self):
-        await self._evt.wait()
-        self._evt = Event()
-        msg,self._msg = self._msg,None
-        return msg
+    async def read(self, t=100):
+        """
+        Wait for new data.
+        If none arrive after 1/10th second, poll the remote side.
+
+        If this method is called before "run", it waits for that to start.
+        """
+        if self._rd is not None:
+            await self._up.wait()
+            del self._up
+
+        try:
+            return await wait_for_ms(t,anext,self._rd)
+        except TimeoutError:
+            await self._cmd.send("sq",o=(self.__cmd.name, self._link))
 
     async def send(self, msg):
         """

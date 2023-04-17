e87915761d2d56f26c3e9404d9194a8ecf106d56
Matthias Urlichs <matthias@urlichs.de>
2023-04-17 19:17:08 +0200

    Update from MoaT template

--- moat/__init__.py
+++ moat/__init__.py
@@ -1,1 +1,10 @@
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+"""
+Fix-up code for loading MoaT from git. Harmless otherwise.
+"""
+# pylint: disable=cyclic-import
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+from moat._dev_fix import _fix
+
+_fix()

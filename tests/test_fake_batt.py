c44980dfd1f8f7a675a5d5ae7ed4c89780249846
Matthias Urlichs <matthias@urlichs.de>
2023-04-19 08:02:24 +0200

    Linted

--- tests/test_fake_batt.py
+++ tests/test_fake_batt.py
@@ -5,23 +5,21 @@ import pytest
 
 pytestmark = pytest.mark.anyio
 
-import os
-import sys
-
-import anyio
 from moat.micro._test import mpy_client, mpy_server
-from moat.micro.compat import sleep_ms
-from moat.util import NotGiven
 
 TT = 250  # XXX assume that this is OK
 
 
 async def test_bms(tmp_path):
+    "Basic BMS test"
     ended = False
     async with mpy_server(tmp_path) as obj:
         async with mpy_client(obj) as req:
             res = await req.send("ping", "hello")
             assert res == "R:hello"
             ended = True
+
+            s = await req.send(["loc","bat1","state"])
+            print(s)
 
     assert ended

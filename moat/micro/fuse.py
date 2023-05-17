3a1b8c986443426966cce7ef5b7311f4bf9024aa
Matthias Urlichs <matthias@urlichs.de>
2023-05-17 16:57:56 +0200

    FUSE: set max_read, otherwise we have a problem
    pyfuse3 3.2

--- moat/micro/fuse.py
+++ moat/micro/fuse.py
@@ -811,7 +811,8 @@ async def wrap(link: SubDispatch, path: Path, blocksize=0, debug=1):
     logger.debug("Mounting...")
     fuse_options = set(pyfuse3.default_options)  # pylint: disable=I1101
     fuse_options.add("fsname=moat_fs")
-    # fuse_options.add(f'max_read={operations.max_read}')
+    fuse_options.add(f'max_read={operations.max_read}')
+    # fuse_options.add(f'max_write={operations.max_write}')
     if debug > 1:
         fuse_options.add("debug")
     pyfuse3.init(operations, str(path), fuse_options)  # pylint: disable=I1101

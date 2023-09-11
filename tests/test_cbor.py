05dff9f10b86d1a303236eb2bcdaccf4c599fe83
Matthias Urlichs <matthias@urlichs.de>
2023-09-11 21:05:33 +0200

    add (disabled for now) CBOR test

---
+++ tests/test_cbor.py
@@ -0,0 +1,49 @@
+"""
+Some rudimentary tests for packing
+"""
+
+# pylint: disable=missing-function-docstring
+
+import pytest
+
+pytestmark = pytest.mark.skip
+
+from moat.util import as_proxy, attrdict, packer, unpacker
+
+
+class Bar:
+    "A proxied object"
+
+    def __init__(self, x):
+        self.x = x
+
+    def __eq__(self, other):
+        return self.x == other.x
+
+
+# needs "replace" because testing re-imports
+@as_proxy("fu", replace=True)
+class Foo(Bar):
+    "A proxied class"
+    pass  # pylint: disable=unnecessary-pass
+
+
+_val = [
+    Foo(42),
+    attrdict(x=1, y=2),
+]
+
+
+def test_basic():
+    for v in _val:
+        w = unpacker(packer(v, cbor=True), cbor=True)
+        assert v == w, (v, w)
+
+
+def test_bar():
+    b = Bar(95)
+    as_proxy("b", b, replace=True)
+    c = unpacker(packer(b, cbor=True), cbor=True)
+    assert b == c
+    with pytest.raises(TypeError):
+        packer(Bar(94), cbor=True)

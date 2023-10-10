3e3e87415c55f02fb248f02af68705f4ac591164
Matthias Urlichs <matthias@urlichs.de>
2023-10-10 03:01:37 +0200

    Extract the exit code from exceptiongroups

--- moat/__main__.py
+++ moat/__main__.py
@@ -2,5 +2,22 @@
 Code for "python3 -mmoat" when running in the MoaT source tree.
 """
 from moat.main import cmd
+import sys
+import trio
 
-cmd()
+ec = 0
+
+def _leaves(exc):
+    if isinstance(exc, BaseExceptionGroup):
+        for e in exc.exceptions:
+            yield from _leaves(e)
+    else:
+        yield exc
+
+try:
+    cmd()
+except* SystemExit as ex:
+    for e in _leaves(ex):
+        ec |= e.code
+
+sys.exit(ec)

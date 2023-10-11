e6e33683535c51f833513568da7c2bc32b3b1479
Matthias Urlichs <matthias@urlichs.de>
2023-10-11 07:56:46 +0200

    Split Reliable test
    Going through all permutations took too long

--- tests/test_reliable.py
+++ tests/test_reliable.py
@@ -59,9 +59,37 @@ class Recv(Head):
 @pytest.mark.parametrize("qlen1", [2, 20])
 @pytest.mark.parametrize("qlen2", [2, 20])
 @pytest.mark.parametrize("window", [4, 8, 20])
-@pytest.mark.parametrize("loss", [0, 0.1, 0.7])
-async def test_basic(qlen1, qlen2, window, loss):
+async def test_basic(qlen1, qlen2, window):
     "basic test for Reliable channel"
+    loss=0
+    u1 = Loopback(qlen=qlen1)
+    u2 = Loopback(qlen=qlen2)
+    u1.link(u2)
+    u2.link(u1)
+#   u1 = LogMsg(u1, dict( txt="L1"))
+#   u2 = LogMsg(u2, dict( txt="L2"))
+    u1 = ReliableMsg(u1, dict(_nowait=True, retries=999, window=window, timeout=100, persist=True))
+    u2 = ReliableMsg(u2, dict(_nowait=True, retries=999, timeout=100))
+#   u1 = LogMsg(u1, dict(txt="U1"))
+#   u2 = LogMsg(u2, dict(txt="U2"))
+    u1 = Xmit(u1, {})
+    u2 = Recv(u2, {})
+
+    async with TaskGroup() as tg:
+        async with u1,u2:
+            await tg.spawn(u1.run)
+            await tg.spawn(u2.run)
+            await u1.done.wait()
+            await u2.done.wait()
+    assert u1.n == 10
+    assert u2.n == 10
+
+@pytest.mark.parametrize("window", [4, 8])
+@pytest.mark.parametrize("loss", [0.1, 0.7])
+async def test_lossy(window, loss):
+    "basic test for Reliable channel"
+    qlen1=5
+    qlen2=5
     u1 = Loopback(qlen=qlen1, loss=loss)
     u2 = Loopback(qlen=qlen2, loss=loss)
     u1.link(u2)
@@ -70,8 +98,8 @@ async def test_basic(qlen1, qlen2, window, loss):
 #   u2 = LogMsg(u2, dict( txt="L2"))
     u1 = ReliableMsg(u1, dict(_nowait=True, retries=999, window=window, timeout=100, persist=True))
     u2 = ReliableMsg(u2, dict(_nowait=True, retries=999, timeout=100))
-    u1 = LogMsg(u1, dict(txt="U1"))
-    u2 = LogMsg(u2, dict(txt="U2"))
+#   u1 = LogMsg(u1, dict(txt="U1"))
+#   u2 = LogMsg(u2, dict(txt="U2"))
     u1 = Xmit(u1, {})
     u2 = Recv(u2, {})
 

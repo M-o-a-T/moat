975e2c617e052a18e9e1779a686bf9368e051bf6
Matthias Urlichs <matthias@urlichs.de>
2023-10-11 14:24:39 +0200

    Add basic client/server Unix links, and test

--- moat/micro/app/net/unix.py
+++ moat/micro/app/net/unix.py
@@ -1,9 +1,11 @@
 import anyio
 
 from moat.micro.proto.unix import Link as UnixLink
+from moat.micro.proto.stream import SingleAnyioBuf
 from moat.micro.stacks.console import console_stack
-from moat.micro.cmd.stream import StreamCmd, BaseBBMCmd
+from moat.micro.cmd.stream import StreamCmd, BaseBBMCmd, SingleStreamCmd, ExtStreamCmd
 from moat.micro.cmd.base import BaseCmd
+from moat.micro.cmd.tree import BaseLayerCmd, BaseSubCmd
 from moat.micro.compat import AC_use
 
 
@@ -12,20 +14,21 @@ class Raw(BaseBBMCmd):
     def stream(self):
         return UnixLink(self.port)
 
-class Link(StreamCmd):
+class Link(SingleStreamCmd):
     async def stream(self):
-        return await AC_use(self, console_stack(UnixLink(self.port), self.cfg))
+        return await AC_use(self, console_stack(UnixLink(self.cfg["port"]), self.cfg))
 
 
-class Port(BaseCmd):
+class Port(BaseSubCmd):
     async def _handle(self, client):
-        fd = client.fileno()
+        fd = client.extra(anyio.abc.SocketAttribute.raw_socket).fileno()
         async with console_stack(SingleAnyioBuf(client), self.cfg) as s:
 
-            sc = SingleStreamCmd(s)
-            self._parent.attach(fd, sc)
+            sc = ExtStreamCmd(s)
+            await self.attach(fd, sc)
+            await sc._stopped.wait()
 
     async def run(self):
         listener = await anyio.create_unix_listener(self.cfg["port"])
         self.set_ready()
-        await listener.serve(handle)
+        await listener.serve(self._handle)

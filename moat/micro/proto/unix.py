ab8be28aa00384a28b82ff82ff6dc59ff37b0f53
Matthias Urlichs <matthias@urlichs.de>
2023-10-11 14:12:36 +0200

    rename protocol modules to .proto.ip/.unix

--- moat/micro/proto/unix.py
+++ moat/micro/proto/unix.py
@@ -4,6 +4,8 @@ Support code to connect to a Unix socket.
 
 from __future__ import annotations
 
+import anyio
+
 from moat.micro.compat import AC_use
 from moat.micro.proto.stream import AnyioBuf
 

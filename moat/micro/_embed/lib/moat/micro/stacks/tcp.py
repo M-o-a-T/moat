f9039d6769fe4ae7547c698c190fb73aa761effe
Matthias Urlichs <matthias@urlichs.de>
2023-10-12 13:55:32 +0200

    duh

--- moat/micro/_embed/lib/moat/micro/stacks/tcp.py
+++ moat/micro/_embed/lib/moat/micro/stacks/tcp.py
@@ -1,41 +1,27 @@
 import sys
 
-from asyncio.queues import Queue
+from ..compat import TaskGroup, run_server, AC_use, Event
+from ..proto.stream import SingleAIOBuf
+from ..stacks.util import BaseConnIter
 
-from ..compat import TaskGroup, run_server, AC_use
-from ..proto.stream import AIOStream
-from moat.util import Lockstep
-
-
-async def listen_msg(host="0.0.0.0", port=0): # type: Iterator[BaseMsg]
+class TcpIter(BaseConnIter):
     """
-    This async context manager returns an async iterator for new network
-    connections on a given host/port listener socket.
-
-    The caller is responsible for properly closing each stream:
-    its (async) context must be entered exactly once.
+    A connection iterator for TCP sockets
     """
+    def __init__(self, host, port):
+        super().__init__()
+        self.host = host
+        self.port = port
 
+    async def accept(self) -> Never:
+        async with TaskGroup() as tgx:
+            evt = Event()
+            async def rdy(evt):
+                await evt.wait()
+                self.set_ready()
+            await tgx.spawn(rdy,evt)
 
-    srv = None
-    n = 0
-    tg = await AC_use(self, TaskGroup())
-    await AC_use(self, tg.cancel)
+            await run_server(self._handle, self.host, self.port, evt=evt)
 
-    class Getter(Lockstep):
-        async def __anext__(self):
-            s = await super().__anext__()
-            return AIOStream(self.s)
-
-        async def put(s, rs):
-            assert s == rs
-            try:
-                await super().put(s)
-            except BaseException:
-                s.close()
-                await s.wait_closed()
-                raise
-
-    await tg.spawn(run_server, enq, host, port, _name="run_server")
-
-    return Getter()
+    async def _handle(self, conn):
+        await self.add_conn(SingleAIOBuf(conn))

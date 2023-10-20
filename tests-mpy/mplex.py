17c0b0be4ebd24749ab4cbcaf01f42d24cb93df2
Matthias Urlichs <matthias@urlichs.de>
2023-10-20 12:48:23 +0200

    deal with missing PYTHONPATH

--- tests-mpy/mplex.py
+++ tests-mpy/mplex.py
@@ -15,27 +15,28 @@ except OSError:
 else:
     d+=os.sep+"micro"
 
-for p in os.getenv("PYTHONPATH").split(":"):
-    if p == ".":
-        p = h
-    elif p.startswith("."):
-        p = h+"/"+p
-    ep = p+"/moat/micro/_embed"
-    try:
-        os.stat(ep)
-    except OSError:
-        # print ("NO",ep,file=sys.stderr)
-        pass
-    else:
-        # print ("YS",ep,file=sys.stderr)
-        sys.path.insert(0,ep)
+if os.getenv("PYTHONPATH"):
+    for p in os.getenv("PYTHONPATH").split(":"):
+        if p == ".":
+            p = h
+        elif p.startswith("."):
+            p = h+"/"+p
+        ep = p+"/moat/micro/_embed"
         try:
-            os.stat(ep+"/lib")
+            os.stat(ep)
         except OSError:
+            # print ("NO",ep,file=sys.stderr)
             pass
         else:
-            # print ("YS",ep+"/lib",file=sys.stderr)
-            sys.path.insert(0,ep+"/lib")
+            # print ("YS",ep,file=sys.stderr)
+            sys.path.insert(0,ep)
+            try:
+                os.stat(ep+"/lib")
+            except OSError:
+                pass
+            else:
+                # print ("YS",ep+"/lib",file=sys.stderr)
+                sys.path.insert(0,ep+"/lib")
 sys.path.insert(0,"./stdlib")
 sys.path.insert(0,".")
 

ec7cde3fa605b079475600609170bc672cbdf2be
Matthias Urlichs <matthias@urlichs.de>
2023-10-20 11:58:36 +0200

    app for redirecting

---
+++ moat/micro/_embed/lib/app/remote.py
@@ -0,0 +1,46 @@
+"""
+Remote port access apps
+"""
+from __future__ import annotations
+
+from moat.micro.compat import AC_use
+from moat.micro.cmd.stream import BaseBBMCmd, BaseCmdMsg
+from moat.micro.stacks.console import console_stack
+from moat.micro.part.serial import Serial
+
+class Raw(BaseBBMCmd):
+    """
+    Link to a stream that's someplace else.
+
+    This app forwards read/write requests to somewhere else.
+    """
+    async def stream(self) -> BaseBuf:
+        "returns the link"
+        return await AC_use(self, self.root.sub_at(*self.cfg["path"]))
+
+
+class Fwd(BaseCmd):
+    """
+    Link to a stream that's someplace else.
+
+    This app forwards to somewhere else.
+    """
+    async def setup(self):
+        "create a subdispatcher"
+        await super().setup()
+        self.sd = self.root.sub_at(*self.cfg["path"])
+
+    def dispatch(self, action, msg, **kw) -> Awaitable:
+        "call via the subdispatcher"
+        await self.sd.dispatch(action, msg, **kw)
+
+
+class Link(BaseCmdMsg):
+    """
+    Sends/receives MoaT messages using a stream that's somewhere else.
+    """
+    async def stream(self) -> BaseMsg:
+        "returns the stack-wrapped link"
+        sd = AC_use(self, self.root.sub_at(*self.cfg["path"]))
+        return await AC_use(self, console_stack(sd, self.cfg))
+

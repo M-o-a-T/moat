f922a930d79f50097337adc19cde6effd2accdcf
Matthias Urlichs <matthias@urlichs.de>
2023-10-25 15:18:39 +0200

    Update from MoaT template

--- moat/__init__.py
+++ moat/__init__.py
@@ -1,6 +1,3 @@
-# noqa:D104 pylint:disable=missing-module-docstring
-from __future__ import annotations
-
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 from moat._dev_fix import _fix

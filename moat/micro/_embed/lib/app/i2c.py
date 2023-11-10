24e19452678fbec9b272eb9db7ade24c479b0872
Matthias Urlichs <matthias@urlichs.de>
2023-11-10 11:54:42 +0100

    I²C: bake the pins into the config file

--- moat/micro/_embed/lib/app/i2c.py
+++ moat/micro/_embed/lib/app/i2c.py
@@ -17,73 +17,79 @@ class Cmd(BaseCmd):
 
     Warning: This is synchronous, thus subjects the whole system to
     arbitrary slowdowns if the destination device employs clock stretching.
+
+    Config::
+
+        c: 3  # pin# of control
+        d: 4  # pin# of data
+        cx: {}  # additional machine.Pin params for control pin
+        dx: {}  # additional machine.Pin params for data pin
+        f: 100000  # frequency, Hz
+        s: false  # use bit-banging I²C driver?
+        t: 500 # bus timeout, msec
     """
-
-    _bus_cache = None  # c,d > busobj
-
-    def __init__(self, cfg):
-        super().__init__(cfg)
-        self._bus_cache = dict()
-
-    def _bus(self, cd, drop=False):
-        # (c,d) > bus
-        c, d = cd
-        cd = (c, d)
-        if drop:
-            return self._bus_cache.pop(cd)
-        else:
-            return self._bus_cache[cd]
-
-    def _del_cd(self, cd):
-        self._bus(cd, True)
+    _bus = None
 
     async def cmd_reset(self, p=None):  # noqa:ARG002
-        "close all"
         # for _ in self._bus_cache.values():
         #     pass  # XXX b.close()
         self._bus_cache = dict()
 
-    async def cmd_open(self, c, d, cx={}, dx={}, f=1000000, t=1000000, s=False):  # noqa:B006
+    async def setup(self):
         """
         Open a bus.
-        @c, @d: control and data pins.
-        @cx, @dx: additional params for the pins.
-        @f: clock frequency in Hz.
-        @t: bus timeout in milliseconds.
-        @s: flag: use software I²C driver?
         """
-        cd = (c, d)
-        if cd in self._bus_cache:
-            pass  # close it
-        c = machine.Pin(c, **cx)
-        d = machine.Pin(d, **dx)
-        bus = (machine.SoftI2C if s else machine.I2C)(scl=c, sda=d, freq=f, timeout=t * 1000)
-        self._bus_cache[cd] = bus
-        return cd
+        await super().setup()
+        self._setup()
 
-    async def cmd_rd(self, cd, i, n=16):
+    async def reload(self):
+        self._teardown()
+        self._setup()
+        await super().reload()
+
+    def _setup(self):
+        c = machine.Pin(cfg["c"], **cfg.get("cx",{}))
+        d = machine.Pin(cfg["d"], **cfg.get("dx",{}))
+        self._bus = (machine.SoftI2C if cfg.get("soft",False) else machine.I2C)(
+            scl=c,
+            sda=d,
+            freq=cfg.get(f, 100000),
+            timeout=cfg.get("t", 1000) * 1000,
+        )
+
+    async def teardown(self):
+        self._teardown()
+        await super().teardown()
+
+    def _teardown(self):
+        if self._bus is None:
+            try:
+                self._bus.deinit()
+            except AttributeError:
+                pass
+
+
+    async def cmd_rd(self, i, n=16):
         "read @n bytes from bus @cd at address @i"
-        bus = self._bus(cd)
-        return bus.readfrom(i, n)
+        return self._bus.readfrom(i, n)
 
-    async def cmd_wr(self, cd, i, buf):
+    async def cmd_wr(self, i, buf):
         "write @buf to bus @cd at address @i"
-        bus = self._bus(cd)
-        return bus.writeto(i, buf)
+        return self._bus.writeto(i, buf)
 
-    async def cmd_wrrd(self, cd, i, buf, n=16):
-        "write @buf to bus @cd at address @i, then read @n bytes"
-        bus = self._bus(cd)
-        d = bus.writeto(i, buf, False)
+    async def cmd_wrrd(self, i, buf, n=16):
+        """
+        write @buf to bus @cd at address @i, then read @n bytes.
+
+        Returns -x if only x bytes could be written.
+        """
+        bus = self._bus
+        d = self._bus.writeto(i, buf, False)
         if d < len(buf):
+            bus.stop()
             return -d
-        return bus.readfrom(i, n)
+        return self._bus.readfrom(i, n)
 
-    async def cmd_cl(self, cd):
-        "close bus @cd"
-        self._del_cd(cd)
-
-    async def cmd_scan(self, cd):
-        "scan bus @cd"
-        bus = self._bus(cd)
-        return bus.scan()
+    async def cmd_scan(self):
+        "scan the bus"
+        return self._bus.scan()

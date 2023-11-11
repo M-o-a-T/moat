ff5abf0a9b84a1e82c9507b212e9edb01a0d525a
Matthias Urlichs <matthias@urlichs.de>
2023-11-11 19:31:32 +0100

    missing modules

--- moat/micro/_embed/lib/manifest.py
+++ moat/micro/_embed/lib/manifest.py
@@ -80,8 +80,10 @@ try:
     require("functools")
     require("pprint")
     require("types")
+    require("typing")
     require("__future__")
 
+    module("msgpack.py", opt=0)
     module("serialpacker.py", opt=0)
     module("async_queue.py", opt=0)
     package("moat", opt=0)

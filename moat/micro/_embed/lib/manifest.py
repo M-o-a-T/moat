9515db8a8e6c4707e9121cbc52b223bd83c23339
Matthias Urlichs <matthias@urlichs.de>
2023-11-13 12:03:55 +0100

    more stuff for esp8266
    so far, not enough memory

--- moat/micro/_embed/lib/manifest.py
+++ moat/micro/_embed/lib/manifest.py
@@ -1,17 +1,5 @@
 # flake8: noqa
 
-# update version file
-
-require("copy")
-require("collections")
-require("collections-deque")
-require("contextlib")
-require("functools")
-require("pprint")
-require("types")
-require("typing")
-require("__future__")
-
 module("msgpack.py", opt=0)
 module("serialpacker.py", opt=0)
 module("async_queue.py", opt=0)
---
+++ moat/micro/_embed/manifest.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+
+freeze("$(PORT_DIR)/modules")
+
+require("copy")
+require("collections")
+require("collections-deque")
+require("contextlib")
+require("functools")
+require("pprint")
+require("types")
+require("typing")
+require("__future__")
+
+include("$(MPY_DIR)/extmod/asyncio")
+
+# TODO only if building with networking
+require("webrepl")
+
+
+import moat.micro._embed.lib
+for p in moat.micro._embed.lib.__path__:
+    print(p)
+    include(p+"/manifest.py")

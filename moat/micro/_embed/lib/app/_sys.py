df83160d26a0389015aec370450df9c7ffbecfb4
Matthias Urlichs <matthias@urlichs.de>
2023-11-14 15:52:36 +0100

    consolidate reboot options

--- moat/micro/_embed/lib/app/_sys.py
+++ moat/micro/_embed/lib/app/_sys.py
@@ -48,56 +48,35 @@ class Cmd(_Cmd):
         t2 = ticks_ms()
         return dict(t=ticks_diff(t2, t1), a=(a2,self.root.i.fa), f=(f2,self.root.i.fm), c=f2 - f1)
 
-    async def cmd_boot(self, code):
+    async def cmd_boot(self, code, m):
         """
-        Reboot the system (soft reset).
+        Reboot MoaT.
 
         @code needs to be "SysBooT".
+
+        @m can be
+            1: immediate soft reset
+            2: immediate hard reset
+            3: return to command line
+            4: "clean" soft reset
         """
         if code != "SysBooT":
             raise RuntimeError("wrong")
 
         async def _boot():
             await sleep_ms(100)
-            machine.soft_reset()
+            if m == 1:
+                machine.soft_reset()
+            elif m == 2:
+                machine.reset()
+            elif m == 3:
+                raise KeyboardInterrupt()
+            elif m == 4:
+                raise SystemExit()
 
         await self.root.tg.spawn(_boot, _name="_sys.boot1")
         return True
 
-    async def cmd_reset(self, code):
-        """
-        Reboot the system (hard reset).
-
-        @code needs to be "SysRsT".
-        """
-        if code != "SysRsT":
-            raise RuntimeError("wrong")
-
-        async def _boot():
-            await sleep_ms(100)
-            machine.reset()
-
-        await self.root.tg.spawn(_boot, _name="_sys.boot2")
-        return True
-
-    async def cmd_stop(self, code):
-        """
-        Terminate MoaT and go back to MicroPython.
-
-        @code needs to be "SysStoP".
-        """
-        # terminate the MoaT stack w/o rebooting
-        if code != "SysStoP":
-            raise RuntimeError("wrong")
-
-        async def _boot():
-            await sleep_ms(100)
-            await self.request.send_nr("link", False)
-            await sleep_ms(100)
-            raise SystemExit
-
-        await self.request._tg.spawn(_boot, _name="_sys.boot3")  # noqa:SLF001
-        return True
 
     async def cmd_machid(self):
         """

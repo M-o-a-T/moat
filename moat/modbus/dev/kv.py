deb033b923b25818961c10c4ae0cc532f6cef9e9
Matthias Urlichs <matthias@urlichs.de>
2023-12-05 13:08:13 +0100

    clean up the KV interface

--- moat/modbus/dev/kv.py
+++ moat/modbus/dev/kv.py
@@ -5,6 +5,8 @@ Support values on MoaT-KV
 import logging
 
 from .device import Register as BaseRegister
+from moat.util import Path, NotGiven
+import anyio
 
 logger = logging.getLogger(__name__)
 
@@ -18,61 +20,115 @@ class Register(BaseRegister):
         super().__init__(*a, **kw)
         self.mt_kv = mt_kv
 
-        if "dest" not in self.data and "src" not in self.data:
+        if self.src is None and self.dest is None:
             if self.data.get("slot", "write") != "write":
-                logger.warning("%s:%s: no source/destination", self.unit, self.path)
+                if getattr(self.data, "_is_orig", False):
+                    logger.warning("%s:%s: no source/destination", self.unit, self.path)
             return
 
         logger.info("%s:%s: Polling", self.unit, self.path)
 
-        if self.data.get("dest"):
-            if self.data.get("slot", "write") == "write":
+        if (dest := self.dest) is not None:
+            slot = self.data.get("slot", None)
+            if slot is None:
                 logger.warning("%s:%s: no read slot", self.unit, self.path)
 
-            elif self.data.dest.mark == "r":
-                tg.start_soon(self.poll_dkv_raw, mt_kv)
-            else:
-                tg.start_soon(self.poll_dkv, mt_kv)
+            if isinstance(dest, Path):
+                dest = (dest,)
 
-        if self.data.get("src"):
-            if self.data.src.mark == "r":
-                tg.start_soon(self.send_dkv_raw, mt_kv)
-            else:
-                tg.start_soon(self.send_dkv, mt_kv)
+            for d in dest:
+                if d.mark == "r":
+                    tg.start_soon(self.to_dkv_raw, d, mt_kv)
+                else:
+                    tg.start_soon(self.to_dkv, d, mt_kv)
 
-    async def poll_dkv(self, mt_kv):
+        if self.src is not None:
+            slot = self.data.get("slot", None) if self.dest is None else None
+            # if a slot is set AND src is set AND dst is not set,
+            # then we want to do a periodic write (keepalive etc.).
+            if self.src.mark == "r":
+                mon = mt_kv.msg_monitor(self.src)
+            else:
+                mon = mt_kv.watch(self.src, fetch=True, max_depth=0)
+
+            if slot in (None, "write"):
+                tg.start_soon(self.from_dkv, mon)
+            else:
+                tg.start_soon(self.from_dkv_p, mon, slot)
+
+    @property
+    def src(self):
+        return self.data.get("src")
+
+    @property
+    def dest(self):
+        return self.data.get("dest")
+
+    async def to_dkv(self, dest, mt_kv):
         """Copy a Modbus value to MoaT-KV"""
         async for val in self:
+#            if "load.goal" in str(dest):
+#                breakpoint()
             logger.debug("%s R %r", self.path, val)
-            await mt_kv.set(self.data.dest, value=val, idem=self.data.get("idem", True))
+            await mt_kv.set(dest, value=val, idem=self.data.get("idem", True))
 
-    async def poll_dkv_raw(self, mt_kv):
+    async def to_dkv_raw(self, dest, mt_kv):
         """Copy a Modbus value to MQTT"""
         async for val in self:
             logger.debug("%s r %r", self.path, val)
-            await mt_kv.msg_send(list(self.data.dest), val)
+            await mt_kv.msg_send(list(dest), val)
 
-    async def send_dkv(self, mt_kv):
+    async def from_dkv(self, mon):
         """Copy a MoaT-KV value to Modbus"""
-        async with mt_kv.watch(self.data.src) as mon:
-            async for val in mon:
+        async with mon as mon_:
+            async for val in mon_:
+                if "value" not in val:
+                    continue
                 logger.debug("%s W %r", self.path, val.value)
                 await self._set(val.value)
 
-    async def send_dkv_raw(self, mt_kv):
-        """Copy an MQTT value to Modbus"""
-        async with mt_kv.msg_monitor(self.data.src) as mon:
-            async for val in mon:
-                logger.debug("%s w %r", self.path, val.value)
-                await self._set(val.value)
+    async def from_dkv_p(self, mon, slot):
+        """Copy an MQTT value to Modbus, with periodic refresh"""
+        evt = anyio.Event()
+        val = NotGiven
+        async def per():
+            nonlocal evt, val
+            while True:
+                if val is NotGiven:
+                    await evt.wait()
+                else:
+                    with anyio.move_on_after(self.cfg.slots[slot]):
+                        await evt.wait()
+                if val is NotGiven:
+                    continue
+
+                await self._set(val)
+
+        async with mon as mon_, anyio.create_task_group() as tg:
+            tg.start_soon(per)
+            first = True
+            async for val_ in mon_:
+                try:
+                    val = val_.value
+                except AttributeError:
+                    if first:
+                        first = False
+                        continue
+                    val = NotGiven
+                logger.debug("%s w %r", self.path, val)
+                evt.set()
+                evt = anyio.Event()
+
 
     async def _set(self, value):
         self.value = value
 
-        if self.data.get("dest") and self.data.get("mirror", False):
-            if self.data.dest.mark == "r":
-                await self.mt_kv.msg_send(list(self.data.dest), self.value)
-            else:
-                await self.mt_kv.set(
-                    self.data.dest, value=self.value, idem=self.data.get("idem", True)
-                )
+        if (dest := self.dest) is not None and self.data.get("mirror", False):
+            if isinstance(dest, Path):
+                dest = (dest,)
+
+            for d in dest:
+                if d.mark == "r":
+                    await self.mt_kv.msg_send(list(d), self.value)
+                else:
+                    await self.mt_kv.set(d, value=self.value, idem=self.data.get("idem", True))

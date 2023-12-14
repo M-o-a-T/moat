0902dcf17a2f50a672e23615e0d7bc370741c9c2
Matthias Urlichs <matthias@urlichs.de>
2023-12-14 17:04:58 +0100

    Fall back to a simple PID controller if the derivative time is zero

--- moat/lib/pid/pid.py
+++ moat/lib/pid/pid.py
@@ -185,12 +185,15 @@ class PID:
         i = min(max(i, self.lower), self.upper)
         # Calculate derivative term
         d = 0.0
-        if self.Kd != 0.0 and self.Tf > 0.0:
-            Kn = 1.0 / self.Tf
-            x = -Kn * self.Kd * e0
-            x = exp(-Kn*dt) * x - Kn * (1.0 - exp(-Kn*dt)) * self.Kd * e
-            d = x + Kn * self.Kd * e
-            e = -(self.Tf/self.Kd) * x
+        if self.Kd != 0.0:
+            if self.Tf > 0.0:
+                Kn = 1.0 / self.Tf
+                x = -Kn * self.Kd * e0
+                x = exp(-Kn*dt) * x - Kn * (1.0 - exp(-Kn*dt)) * self.Kd * e
+                d = x + Kn * self.Kd * e
+                e = -(self.Tf/self.Kd) * x
+            else:
+                d = self.Kd * (e-e0) / dt
         # Set initial value for next cycle
         self.set_initial_value(t, e, i)
         return min(max(p+i+d, self.lower), self.upper)

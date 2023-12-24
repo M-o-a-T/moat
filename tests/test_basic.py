5094f5fb0085bcead0375e9c735cd051293554c3
Matthias Urlichs <matthias@urlichs.de>
2023-12-24 16:45:22 +0100

    fix testcase

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -20,22 +20,31 @@ akumuli_model._test_hook = _hook
 
 
 async def test_basic():  # no autojump
-    async with stdtest(test_0={"init": 125}, n=1, tocks=200) as st, st.client(
-        0
-    ) as client, Tester().run() as t:
-        await st.run(f"akumuli server test -h 127.0.0.1 -p {TCP_PORT}")
-        await st.run("akumuli set test.foo.bar test.one.two whatever foo=bar")
+    async with (
+            stdtest(test_0={"init": 125}, n=1, tocks=200) as st,
+            st.client(0) as client,
+            Tester().run() as t,
+            ):
+        await st.run(f"akumuli test add -h 127.0.0.1 -p {TCP_PORT}")
+        await client.set(P("test.one.two"), value=41)
+        await st.run("akumuli test at test.foo.bar add test.one.two whatever foo=bar")
         aki = await AkumuliRoot.as_handler(client)
-        st.tg.start_soon(task, client, client._cfg.akumuli, aki["test"])
-        await anyio.sleep(0.5)
+        aki._cfg.server_default.port = TCP_PORT
+        st.tg.start_soon(task, client, aki._cfg, aki["test"])
+        await anyio.sleep(1)
+        await aki["test"].flush()
         await client.set(P("test.one.two"), value=42)
-        await anyio.sleep(0.5)
+        await anyio.sleep(0.3)
+        await aki["test"].flush()
+        await anyio.sleep(0.8)
+        await client.set(P("test.one.two"), value=43)
+        await anyio.sleep(0.3)
         await aki["test"].flush()
 
         n = 0
         async for x in t.get_data("whatever", tags={}, t_start=time() - 1000, t_end=time() + 1000):
             n += 1
-            assert x[0] == 42
-            assert abs(time() - x[1].timestamp()) < 10
-        assert n == 1
+            assert x.value in (41,42,43)
+            assert abs(time() - x.time) < 10
+        assert n > 1
         pass

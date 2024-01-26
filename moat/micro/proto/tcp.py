5c2ca63fef80187054366e7014b2f2f161b84e3b
Matthias Urlichs <matthias@urlichs.de>
2024-01-26 17:41:19 +0100

    Retry connecting via TCP
    so if we don't die if the destination is slow when we start it

--- moat/micro/proto/tcp.py
+++ moat/micro/proto/tcp.py
@@ -5,8 +5,9 @@ Support code to connect to a TCP server.
 from __future__ import annotations
 
 import anyio
+import errno
 
-from moat.micro.compat import AC_use
+from moat.micro.compat import AC_use, log
 from moat.micro.proto.stream import AnyioBuf
 
 
@@ -15,32 +16,40 @@ class Link(AnyioBuf):
     A channel that connects to a remote TCP socket.
     """
 
-    def __init__(self, host: str, port: int):
+    def __init__(self, host: str, port: int, retry: dict = {}):
         self.host = host
         self.port = port
+        self.retry = retry
 
     async def stream(self):
         """
         Open a TCP connection.
-
-        TODO make reconnect behavior on ECONNREFUSED configurable
         """
 
         n = 0
-        sl = 0.1
-        while True:
-            try:
-                s = await anyio.connect_tcp(self.host, self.port)
-            except OSError as e:
-                if not isinstance(e, ConnectionRefusedError) and not isinstance(
-                    e.__cause__,
-                    ConnectionRefusedError,
-                ):
-                    raise
-                if n > 10:
-                    raise
-                n += 1
-                await anyio.sleep(sl)
-                sl *= 1.3
-            else:
-                return await AC_use(self, s)
+        retry = self.retry
+        sl = retry.get("delay", 0.1)
+        er = None
+        try:
+            with anyio.fail_after(retry.get("timeout", 999)):
+                while True:
+                    try:
+                        s = await anyio.connect_tcp(self.host, self.port)
+                    except OSError as e:
+                        er = e.__cause__ if e.errno is None else e
+                        if er.errno not in {errno.ENETUNREACH,errno.EHOSTUNREACH,errno.ECONNREFUSED}:
+                            raise
+                        if n > retry.get("attempts", 10):
+                            raise TimeoutError from None
+                        if n == 0:
+                            log("Retrying: %s %d, %r", self.host,self.port,er)
+                        n += 1
+                        await anyio.sleep(sl)
+                        sl *= retry.get("backoff", 1.3)
+                    else:
+                        if n:
+                            log("Success: %s %d", self.host,self.port)
+                        return await AC_use(self, s)
+        except TimeoutError:
+            log("Fail: %s %d, %r", self.host,self.port,er)
+            raise

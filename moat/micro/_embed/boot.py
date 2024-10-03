d77a8e8e81e77896dd62d4b9c7c5071253269505
Matthias Urlichs <matthias@urlichs.de>
2024-10-03 12:03:31 +0200

    Add a second USB serial port on boot

--- moat/micro/_embed/boot.py
+++ moat/micro/_embed/boot.py
@@ -11,4 +11,23 @@ except ValueError:
     pass
 sys.path.insert(0, "/lib")
 
+import moat  # just for the namespace
+if not hasattr(moat,"SERIAL"):
+    try:
+        import os
+        import time
+        import usb.device
+
+        from usb.device.cdc import CDCInterface
+        moat.SERIAL = CDCInterface()
+        try:
+            moat.SERIAL.init(timeout=0)
+            usb.device.get().init(moat.SERIAL, builtin_driver=True)
+        except Exception as exc:
+            del moat.SERIAL
+            raise
+
+    except Exception as exc:
+        print("No serial", exc)
+
 print("\n*** MoaT ***\n", file=sys.stderr)

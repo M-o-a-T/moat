795a4b63c5ca9cbadb151518dadd413f88405bb3
Matthias Urlichs <matthias@urlichs.de>
2024-11-07 13:22:22 +0100

    Fix tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,12 @@
-import pytest
+"""
+Basic codec tests
+"""
+
+from __future__ import annotations
 
 from moat.lib.codec import NoCodecError, get_codec
+
+import pytest
 
 
 def test_noop():
@@ -28,19 +34,38 @@ def test_utf8():
         c.encode(42)
 
 
-def test_json():
-    import json
+objs = (
+    "Foo",
+    True,
+    False,
+    None,
+    123,
+    99.5,
+    {"one": "two"},
+    [12, "three four", 56],
+)
 
+@pytest.mark.parametrize("obj",objs)
+def test_json(obj):
+    import json
     c = get_codec("json")
-    for obj in (
-        "Foo",
-        True,
-        False,
-        None,
-        123,
-        99.5,
-        {"one": "two"},
-        [12, "three four", 56],
-    ):
-        assert json.loads(c.encode(obj).decode("utf-8")) == obj
-        assert c.decode(json.dumps(obj).encode("utf-8")) == obj
+
+    assert json.loads(c.encode(obj).decode("utf-8")) == obj
+    assert c.decode(json.dumps(obj).encode("utf-8")) == obj
+
+@pytest.mark.parametrize("obj",objs)
+def test_msgpack(obj):
+    import msgpack
+    c = get_codec("msgpack")
+
+    assert msgpack.unpackb(c.encode(obj), use_list=True) == obj
+    assert c.decode(msgpack.packb(obj)) == obj
+
+
+@pytest.mark.parametrize("obj",objs)
+def test_cbor(obj):
+    import cbor2
+    c = get_codec("cbor")
+
+    assert cbor2.loads(c.encode(obj)) == obj, (obj, c.encode(obj))
+    assert c.decode(cbor2.dumps(obj)) == obj, (obj, cbor2.dumps(obj))

5fb7f49059db9666835522dce73fee09620203cc
Matthias Urlichs <matthias@urlichs.de>
2024-11-14 13:25:34 +0100

    more fun with meta

--- moat/link/meta.py
+++ moat/link/meta.py
@@ -9,12 +9,19 @@ import time
 from attrs import define, field
 from moat.util import NotGiven
 from moat.util.cbor import StdCBOR
-from base64 import b85decode,b85encode
+from moat.lib.codec.proxy import as_proxy
+from base64 import b85decode, b85encode
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any, Self
 
 _codec = StdCBOR()
 
+
 def _gen(i):
-    ii = i+1
+    ii = i + 1
 
     def get(self):
         self._len(ii)
@@ -24,33 +31,43 @@ def _gen(i):
         self._len(ii)
         self[i] = val
 
-    return property(get,set)
+    return property(get, set)
 
 
+def _Meta(a, kw):
+    res = MsgMeta(name=NotGiven)
+    res.__setstate__(a, kw)
+    return res
+
+
+@as_proxy("_MM", factory=_Meta)
 @define(kw_only=True)
 class MsgMeta:
     """
-    This class encapsulates a message's metadata, transmitting them
-    as an array (possibly followed by a dict), encoding non-strings
-    with CBOR and Base85/btoa.
+    This class encapsulates a message's metadata by encoding them in a
+    hopefully-space-saving way.
+
+    XXX: either create a dictionary for origin names, as there aren't many,
+    or rely on data compression?
 
     Currently defined offsets:
 
     * 0: origin: a string declaring which subsystem created a message.
     * 1: timestamp: the Unix timestamp when the message was originally
          created.
-    
+
     You can use indexing to address any other array or keyword value.
     Deleting an array member sets it to ``None``.
 
     """
-    a:list[Any] = field(factory=list)
-    kw:dict[str,Any] = field(factory=dict)
+
+    a: list[Any] = field(factory=list)
+    kw: dict[str, Any] = field(factory=dict)
 
     origin = _gen(0)
     timestamp = _gen(1)
 
-    def __init__(self, /, name:str|NotGiven|None=None, **kwargs):
+    def __init__(self, /, name: str | NotGiven | None = None, **kwargs):
         filtered = {
             attribute.name: kwargs[attribute.name]
             for attribute in self.__attrs_attrs__
@@ -58,34 +75,40 @@ class MsgMeta:
         }
         self.__attrs_init__(**kwargs)
         self._clean(name)
-    
+
     def __getitem__(self, k):
-        if isinstance(k,int):
+        if isinstance(k, int):
             return self.a[k]
         else:
             return self.kw[k]
 
+    def __getstate__(self):
+        return self.a, self.kw
+
+    def __setstate__(self, a, kw):
+        self.a = a
+        self.kw = kw
+
     def __setitem__(self, k, v):
-        if isinstance(k,int):
-            if isinstance(k, slice) or k < 0:
-                raise ValueError("Only use positive indices")
-            if isinstance(v,str):
-                if ("|" in v or "\\" in v):
-                    raise ValueError("Value may not contain '|' or '\\'")
-            elif k == 0:
+        if isinstance(k, slice):
+            raise ValueError("Only use positive indices")
+        if isinstance(k, int):
+            if k < 0:
+                raise KeyError(k)  # only positive indices
+            elif k == 0 and (not isinstance(v, str) or v == "" or "|" in v or "\\" in v):
                 raise ValueError("First item must be a string")
-            self._len(k+1)
+            self._len(k + 1)
             self.a[k] = v
         else:
             self.kw[k] = v
 
     def __delitem__(self, k):
-        if isinstance(k,int):
+        if isinstance(k, int):
             self.a[k] = None
         else:
             del self.kw[k]
 
-    def _clean(self, name:str|None):
+    def _clean(self, name: str | None):
         if name is NotGiven:
             return
         if name is None:
@@ -108,7 +131,7 @@ class MsgMeta:
             res.append(self.kw)
         return res
 
-    def _unmap(self, data:list[any]):
+    def _unmap(self, data: list[any]):
         if isinstance(data[-1], dict):
             self.kw = data.pop()
         self.a = data
@@ -119,56 +142,83 @@ class MsgMeta:
         except StopIteration:
             pass
 
-    def encode(self):
+    def encode(self) -> str:
+        """
+        Encode this object to a string.
+
+        Elements are either UTF-8 strings, introduced by ``|``, or
+        some other data, introduced by ``\\``. Strings that include
+        either of these characters are treated as "other data".
+
+        Empty strings are encoded as zero-length "other data" elements.
+        A value of ``None`` is encoded as an empty string.
+
+        The first item is not marked explicitly.
+        It must be a non-empty string.
+
+        Other data are encoded to CBOR, then base85-encoded
+        (btoa alphabet).
+
+        The last element may be a dict with free-form content.
+        """
         data = self._map()
         res = []
         for d in data:
-            if isinstance(d,str):
+            if isinstance(d, str) and d != "" and "|" not in d and "\\" not in d:
+                if not d and not res:
+                    raise ValueError("No empty origins")
                 if res:
                     res.append("|")
-                res.append(d)
-            else:
-                if not res:
-                    raise ValueError("First entry must be a string")
-                res.append("\\")
+                if d is not None:
+                    res.append(d)
+                continue
+
+            if not res:
+                raise ValueError("No non-string origins")
+            res.append("\\")
+            if d != "":
                 res.append(b85encode(_codec.encode(d)).decode("utf-8"))
         return "".join(res)
 
     @classmethod
-    def decode(cls, name:str, data):
+    def decode(cls, name: str, data) -> Self:
+        """
+        Decode a string to a `MsgMeta` object.
+
+        Reverses the effect of `encode`.
+        """
         res = cls(name)
         ddec = []
 
-        colon = False
+        encoded = False
         while data:
-            ncol:bool = None
-            cc:int
+            next_enc: bool = None
+            cc: int
 
             c1 = data.find("|")
             c2 = data.find("\\")
             if c1 == -1:
                 cc = c2
-                ncol = True
+                next_enc = True
             elif c2 == -1:
                 cc = c1
-                ncol = False
+                next_enc = False
             else:
-                cc = min(c1,c2)
-                ncol = cc==c2
+                cc = min(c1, c2)
+                next_enc = cc == c2
 
             if cc == -1:
-                d,data = data,None
+                d, data = data, None
             else:
-                d,data = data[:cc],data[cc+1:]
-            if colon:
-                d = _codec.decode(b85decode(d.encode("utf-8")))
+                d, data = data[:cc], data[cc + 1 :]
+            if encoded:
+                if d != "":
+                    d = _codec.decode(b85decode(d.encode("utf-8")))
+            elif d == "":
+                d = None
             ddec.append(d)
-            colon = ncol
+            encoded = next_enc
 
         res._unmap(ddec)
         res._clean(name)
         return res
-        
-
-        
-
--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,5 +1,5 @@
-"""Basic tests for nodes
-"""
+"""Basic tests for nodes"""
+
 from __future__ import annotations
 
 import pytest
@@ -8,6 +8,8 @@ from base64 import b85encode
 
 from moat.link.meta import MsgMeta
 from moat.lib.codec import get_codec
+from moat.lib.codec.proxy import wrap_obj, unwrap_obj
+
 
 def test_basic():
     codec = get_codec("cbor")
@@ -18,7 +20,7 @@ def test_basic():
     n = MsgMeta(name=name)
     tt = 12345
     assert n.origin == name
-    assert time.time()-.1 <= n.timestamp  <= time.time()+.1
+    assert time.time() - 0.1 <= n.timestamp <= time.time() + 0.1
     n.timestamp = tt
 
     ts = b85encode(codec.encode(tt)).decode("utf-8")
@@ -30,15 +32,17 @@ def test_basic():
     assert nn.a[0] == n.origin == nn.origin
     assert nn.a[1] == n.timestamp == nn.timestamp == tt
 
+
 def test_dict():
     codec = get_codec("cbor")
 
-    md = "\\"+b85encode(codec.encode({"yes":True,"no":False})).decode("utf-8")
+    md = "owch\\" + b85encode(codec.encode({"yes": True, "no": False})).decode("utf-8")
     nn = MsgMeta.decode("duh", md)
     assert nn.kw["yes"] is True
     assert nn.kw["no"] is False
-    assert nn.origin == "duh"
-    assert time.time()-.1 <= nn.timestamp  <= time.time()+.1
+    assert nn.origin == "owch"
+    assert time.time() - 0.1 <= nn.timestamp <= time.time() + 0.1
+
 
 def test_bad():
     n = MsgMeta("duh")
@@ -46,13 +50,32 @@ def test_bad():
         n.origin = "here|now"
     # works in a dict
     n["doc"] = "escaped\\ntext"
-    # but not in the array
+    # and somewhere in the array
+    n[2] = "escaped\\ntext"
+    # but not as the origin
     with pytest.raises(ValueError):
-        n[2] = "escaped\\ntext"
+        n[0] = "more|text"
+    # negative indices are bad
+    with pytest.raises(KeyError):
+        n[-1] = "Hello"
+    with pytest.raises(ValueError):
+        n[1:2] = ("Hello",)
     # this works
     n["doc"] = b"escaped\\ntext"
     # but this doesn't
     with pytest.raises(ValueError):
         n.origin = b"not text"
     # this is broken but not checked
-    n.timestamp = b'1234'
+    n.timestamp = b"1234"
+
+
+def test_proxy():
+    codec = get_codec("cbor")
+
+    md = "owch\\" + b85encode(codec.encode({"yes": True, "no": False})).decode("utf-8")
+    nn = MsgMeta.decode("duh", md)
+    pr = wrap_obj(nn)
+    assert isinstance(pr, tuple)
+    assert pr[0] == "_MM"
+    n = unwrap_obj(pr)
+    assert n == nn

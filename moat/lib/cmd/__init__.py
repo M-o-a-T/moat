6886bc028d8a5284c90dd9dd5ec221f763faf1aa
Matthias Urlichs <matthias@urlichs.de>
2024-12-20 17:37:51 +0100

    drop CancelledError from here

--- moat/lib/cmd/__init__.py
+++ moat/lib/cmd/__init__.py
@@ -6,11 +6,3 @@ from __future__ import annotations
 
 from ._cmd import *  # noqa: F403
 
-try:
-    from concurrent.futures import CancelledError
-except ImportError:  # nocover
-
-    class CancelledError(Exception):
-        "Basic remote cancellation"
-
-        pass

d19f33d2035a4f40ab405e56a6d2c864fba2375f
Matthias Urlichs <matthias@urlichs.de>
2024-12-23 18:45:38 +0100

    Monorepo. Don't run sys.path fix.

--- moat/src/_templates/moat/__init__.py
+++ moat/src/_templates/moat/__init__.py
@@ -1,5 +1,1 @@
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
-
-from moat._dev_fix import _fix
-
-_fix()
--- tests/cad/cadquery/slider.py
+++ tests/cad/cadquery/slider.py
@@ -6,9 +6,6 @@ import math
 
 if "/src/moat" not in sys.path:
     sys.path.insert(0, "/src/moat")
-    from moat._dev_fix import _fix
-
-    _fix()
 
 sq = math.sqrt(2)
 from moat.cad import Slider

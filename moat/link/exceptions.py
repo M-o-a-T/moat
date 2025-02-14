5e5613144e993b1c077df224deca741dafb33524
Matthias Urlichs <matthias@urlichs.de>
2025-02-14 23:27:00 +0100

    Add moat.link.exceptions.
    Just a clone (re-import actually) of moat.kv.exceptions.

---
+++ moat/link/exceptions.py
@@ -0,0 +1,9 @@
+"""
+This module contains all MoaT-Link exceptions.
+
+Currently this is a re-import of moat.kv.exceptions.
+"""
+from __future__ import annotations
+
+from moat.kv.exceptions import *
+MoaTLinkError = MoaTKVError

b54b3d2bc71cd2c1376cb90a2be6037ddda4a1b9
Matthias Urlichs <matthias@urlichs.de>
2025-03-04 21:49:02 +0100

    fix working with non-sqlite

--- moat/db/util.py
+++ moat/db/util.py
@@ -23,6 +23,8 @@ ensure_cfg("moat.db")
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
+    if "sqlite" not in dbapi_connection.__class__.__name__:
+        return
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()

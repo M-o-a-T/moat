7e1cfc9c676f61f755599e9618ac4fb248472925
Matthias Urlichs <matthias@urlichs.de>
2025-03-04 22:19:00 +0100

    Initial data.

---
+++ moat/db/alembic/versions/0001_initdata.py
@@ -0,0 +1,33 @@
+"""Add initial data
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '0001'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    from moat.db import load
+    from alembic import context
+    config = context.config
+
+    meta=load(config.attributes["config"])
+
+    st=sa.insert(meta.tables["sheet"]).values(id=1)
+    op.execute(st)
+    op.execute("commit")
+
+
+def downgrade() -> None:
+    S=meta.tables["sheet"]
+    st=sa.delete(S).where(S.id==1)
+    op.execute(st)
+    op.execute("commit")
+    pass

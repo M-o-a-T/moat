a2d7798ffedf6c62f53780529872a648334d6e08
Matthias Urlichs <matthias@urlichs.de>
2025-03-23 14:37:35 +0100

    micro: build micropython

--- tests/micro/__init__.py
+++ tests/micro/__init__.py
@@ -1,1 +1,15 @@
 # noqa:D104 pylint:disable=missing-module-docstring
+
+# We need to ensure that the test interpreter exists
+import os
+from pathlib import Path
+def make_upy(force:bool=False):
+	p=Path("ext/micropython/ports/unix")
+	upy = p/"build-standard/micropython"
+	if not force and upy.exists():
+		return
+	if upy.exists():
+		run(["make","clean"], cwd=p, check=True)
+	run(["make"], cwd=p, check=True)
+
+make_upy()

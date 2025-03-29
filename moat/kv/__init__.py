0d35682906dee79a3d4eb128fb08412b0c982ac0
Matthias Urlichs <matthias@urlichs.de>
2025-03-29 14:31:02 +0100

    drop pkg_resources

--- moat/kv/__init__.py
+++ moat/kv/__init__.py
@@ -4,16 +4,16 @@ from __future__ import annotations
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 try:
-    import warning
-    import pkg_resources  # part of setuptools
+    from importlib.metadata import version  
 
-    with warnings.filterwarnings("ignore"):
-        _version = pkg_resources.require("moat.kv")[0].version
-    del pkg_resources
-    del warnings
-
+    _version = version('moat.kv')  
     _version_tuple = tuple(int(x) for x in _version.split("."))
 
 except Exception:  # pragma: no cover
     _version = "0.0.1"
     _version_tuple = (0, 0, 1)
+
+try:
+    del version
+except NameError:
+    pass

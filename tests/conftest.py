31a031cadf0ee130a4038a13060cc633bf0ae6fc
Matthias Urlichs <matthias@urlichs.de>
2025-04-07 17:39:10 +0200

    more modules required for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,5 @@ import sys
 sys.path.insert(0, "../mqttproto/src")
 sys.path.insert(0, "../asyncactor")
 sys.path.insert(0, "../asyncowfs")
+sys.path.insert(0, "../asyncscope")
+sys.path.insert(0, "../range_set")

aa3f82118b569ce39f25b68ce0ff731b8ab5b66a
Matthias Urlichs <matthias@urlichs.de>
2025-05-04 14:19:13 +0200

    Handle EOF

--- moat/kv/client.py
+++ moat/kv/client.py
@@ -491,7 +491,7 @@ class Client:
                         # self.logger.debug("Recv %s", msg)
                         try:
                             await self._handle_msg(msg)
-                        except ClosedResourceError as exc:
+                        except (ClosedResourceError,EOFError) as exc:
                             logger.warning("Reader closed in handler", exc_info=exc)
                             return
 

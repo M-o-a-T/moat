f06a210d34b0155a928d25a65869c83beb88d673
Matthias Urlichs <matthias@urlichs.de>
2025-05-16 02:27:08 +0200

    Add retain flag field

--- moat/link/meta.py
+++ moat/link/meta.py
@@ -70,6 +70,7 @@ class MsgMeta:
     timestamp = _gen(1)
 
     source: Any = None
+    retain: bool|None = field(default=None)
 
     def __init__(self, /, name: str | EllipsisType | None = None, **kwargs):  # pyright: ignore
         vals = {}

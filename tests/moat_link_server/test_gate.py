21d40f1ff45e1e555a31ce24e6aada2bd6afc8f2
Matthias Urlichs <matthias@urlichs.de>
2025-05-20 07:45:57 +0200

    The gateway also monitors this topic

--- tests/moat_link_server/test_gate.py
+++ tests/moat_link_server/test_gate.py
@@ -35,6 +35,7 @@ async def test_gate_mqtt(cfg):
         await sf.tg.start(_dump, sf)
         await sf.server(init={"Hello": "there!", "test": 123})
         c = await sf.client()
+        cc = await sf.client()
         codec=get_codec("json")
 
         await c.d_set(P("test.a.one"),1)
@@ -72,7 +73,7 @@ async def test_gate_mqtt(cfg):
         a= await data_get(c,P("test.a"), out=False)
         assert a==dict(one={"_":1},two={"_":2},three={"_":33})
 
-        b= await backend_get(c,P("test.b"), out=False)
+        b= await backend_get(cc,P("test.b"), out=False)
         assert b==dict(one={"_":b'1'},two={"_":b'22'},three={"_":b'33'})
 
         # now change things
@@ -85,6 +86,6 @@ async def test_gate_mqtt(cfg):
         a= await data_get(c,P("test.a"), out=False)
         assert a==dict(one={"_":111},two={"_":2},three={"_":333},four={"_":444},five={"_":555})
 
-        b= await backend_get(c,P("test.b"), out=False)
+        b= await backend_get(cc,P("test.b"), out=False)
         assert b==dict(one={"_":b'111'},two={"_":b'22'},three={"_":b'333'},four={"_":b'444'},five={"_":b'555'})
 

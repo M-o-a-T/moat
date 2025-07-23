df5c4e824906e0a20c48192326f69ea36f92af8d
Matthias Urlichs <matthias@urlichs.de>
2025-07-23 12:01:41 +0200

    Fix gate startup

--- moat/link/cmd/gate.py
+++ moat/link/cmd/gate.py
@@ -22,42 +22,44 @@ from moat.link.node import Node
 @click.pass_context
 async def cli(ctx, path):
     """
-    This subcommand accesses the data stored in the MoaT-Link server.
+    This subcommand gates MoaT-Link data to/from other channels.
     """
     obj = ctx.obj
     cfg = obj.cfg["link"]
     if obj.port is not None:
         cfg.client.port = obj.port
     obj.conn = await ctx.with_async_resource(Link(cfg))
+    obj.path = path
     if ctx.invoked_subcommand is None:
-        res = await data_get(obj.conn, path, recursive=False)
-    else:
-        obj.path = path
+        await _list(obj)
 
 
 @cli.command()
-@click.argument("name", type=str)
-async def run(obj,name):
+@click.pass_obj
+async def run(obj):
     """
     Run a gateway setup.
     """
-    res = await obj.conn.d.get(P("gate")/name)
-    gate = get_gate(obj.cfg, res)
-    await gate.run(obj.cfg)
-
+    from moat.link.gate import run_gate
+    from moat.util.systemd import as_service
+    async with as_service(obj) as srv:
+        await srv.tg.start(run_gate, obj.cfg, obj.conn, P("gate")+obj.path)
+        srv.set()
+        await anyio.sleep_forever()
 
 
 @cli.command("list")
-@click.argument("name",nargs=-1)
 @click.pass_obj
-async def list_(obj, name:list[str]):
+async def list_(obj):
     """
     List gates / a gate's data.
     """
+    await _list(obj)
 
-    if not name:
+async def _list(obj):
+    if not obj.path:
         seen = False
-        async with obj.conn.d_walk(P(":R.gate"),min_depth=1,max_depth=1) as mon:
+        async with obj.conn.d_walk(P("gate"),min_depth=1,max_depth=1) as mon:
             async for p,d in mon:
                 seen = True
                 print(p[-1])
@@ -65,32 +67,33 @@ async def list_(obj, name:list[str]):
             print("- no data.", file=sys.stderr)
         return
 
-    for n in name:
-        d = obj.conn.d_get(P(":R.gate")/n)
-        yprint(d)
-
-
+    k = {}
     k["recursive"] = True
     k["raw"] = True
     k["empty"] = True
-    await data_get(obj, obj.path, **k)
+    await data_get(obj.conn, P("gate")+obj.path, **k)
 
 
 @cli.command("set", short_help="Add or update a gate entry")
 @attr_args
 @click.option("-S", "--src", type=P, help="Source (in Moat-Link)")
 @click.option("-D", "--dst", type=P, help="Destination (driver specific)")
-@click.option("-d", "--ddriver", type=str, help="Driver")
+@click.option("-d", "--driver", type=str, help="Driver")
 @click.pass_obj
-async def set_(obj, last, new, **kw):
+async def set_(obj, src,dst,driver, **kw):
     """
     Add/change a gateway entry.
     """
 
-    res = await node_attr(obj, obj.path, **kw)
-
-    if obj.meta:
-        yprint(res, stream=obj.stdout)
+    if not len(obj.path):
+        raise SyntaxError("Can't set the top level")
+    if driver:
+        kw["vars_"] += ((P("driver"), driver),)
+    if src:
+        kw["path_"] += ((P("src"), src),)
+    if dst:
+        kw["path_"] += ((P("dst"), dst),)
+    res = await node_attr(obj, P("gate")+obj.path, **kw)
 
 
 class nstr:
@@ -133,79 +136,3 @@ async def delete(obj, before, recursive):
         res = res[0]
     yprint(res, stream=obj.stdout)
 
-
-@cli.command()
-@click.option("-s", "--state", is_flag=True, help="Also get the current state.")
-@click.option("-o", "--only", is_flag=True, help="Value only, nothing fancy.")
-@click.option("-p", "--path-only", is_flag=True, help="Value only, nothing fancy.")
-@click.option("-D", "--add-date", is_flag=True, help="Add *_date entries")
-@click.option("-i", "--ignore", multiple=True, type=P, help="Skip this (sub)tree")
-@click.pass_obj
-async def monitor(obj, state, only, path_only, add_date, ignore):
-    """Monitor a MoaT-Link subtree"""
-
-    cfg = obj.cfg.link
-    seen = False
-    data = Node()
-    plen=len(obj.path)+1
-
-
-    def pr(res):
-        if only:
-            res = res.get("data", NotGiven)
-        elif path_only:
-            res = res["path"]
-        elif not obj.meta:
-            del res["meta"]
-        yprint(res, stream=obj.stdout)
-        print("---", file=obj.stdout)
-        obj.stdout.flush()
-
-    def set(p,d,m):
-        r = dict(path=p,meta=m.repr())
-        if d is not NotGiven:
-            r["data"] = d
-        dd = data.get(p)
-        ddm = dd.meta
-        n = dd.set(..., d, m)
-        if ddm is not None:
-            r["last"] = ts = ddm.timestamp-time.time()
-            r["_last"] = humandelta(ts, ago=True)
-        if n is False:
-            r["_***_"]="Obsolete message. Ignored."
-        return r
-
-    async with (
-        anyio.create_task_group() as tg,
-        obj.conn.monitor(cfg.root+obj.path, subtree=True) as res,
-    ):
-        if state:
-            @tg.start_soon
-            async def get_tree():
-                pl = PathLongener(())
-                async with obj.conn.d.walk(obj.path) as mon:
-                    async for n,p,d,*m in mon:
-                        p = pl.long(n,p)
-                        m = MsgMeta.restore(m)
-                        res = set(p,d,m)
-                        pr(res)
-
-        async for msg in res:
-            mp = Path.build(msg.topic[plen:])
-            if any(p == mp[: len(p)] for p in ignore):
-                continue
-
-            res = set(mp,msg.data,msg.meta)
-            pr(res)
-
-
-@cli.command()
-@click.option("-i", "--infile", type=click.Path(), help="File to read (msgpack).")
-@click.pass_obj
-async def update(obj, infile):
-    """Send a list of updates to a MoaT-KV subtree"""
-    async with MsgReader(path=infile, codec="std-msgpack") as reader:
-        async for msg in reader:
-            if not hasattr(msg, "path"):
-                continue
-            await obj.client.set(obj.path + msg.path, value=msg.value)

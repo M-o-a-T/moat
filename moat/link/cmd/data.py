9851eae454d94928d75aa055bad6eb0629f3ee09
Matthias Urlichs <matthias@urlichs.de>
2025-08-10 14:34:58 +0200

    another port fixup

--- moat/link/cmd/data.py
+++ moat/link/cmd/data.py
@@ -25,8 +25,6 @@ async def cli(ctx, path,meta):
     """
     obj = ctx.obj
     cfg = obj.cfg["link"]
-    if obj.port is not None:
-        cfg.client.port = obj.port
     obj.conn = await ctx.with_async_resource(Link(cfg, name=obj.name))
     obj.meta=meta
     if ctx.invoked_subcommand is None:

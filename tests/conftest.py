aca2e08af2dcfe15517cc95ebd3fe8b14d4cc98d
Matthias Urlichs <matthias@urlichs.de>
2025-10-04 09:52:43 +0200

    Don't use locally-installed packages for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,1 @@
 from __future__ import annotations  # noqa: D100
-
-import sys
-
-sys.path.insert(0, "../mqttproto/src")
-sys.path.insert(0, "../asyncactor")
-sys.path.insert(0, "../asyncowfs")
-sys.path.insert(0, "../asyncscope")
-sys.path.insert(0, "../range_set")

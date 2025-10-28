b1475bc43aa2b7ffd624571a035d1e112176626e
Matthias Urlichs <matthias@urlichs.de>
2025-10-28 08:07:46 +0100

    Fix those baguette builds
    Exact reason unknown but whatever

--- moat/micro/_embed/boards/esp32/s3_baguette/mpconfigboard.cmake
+++ moat/micro/_embed/boards/esp32/s3_baguette/mpconfigboard.cmake
@@ -2,6 +2,8 @@ set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
-    # boards/sdkconfig.s3
+    boards/sdkconfig.usb
+    boards/sdkconfig.ble
+    boards/sdkconfig.spiram_sx
     ${MICROPY_BOARD_DIR}/sdkconfig.board
 )
--- moat/micro/_embed/boards/esp32/s3_baguette/mpconfigboard.h
+++ moat/micro/_embed/boards/esp32/s3_baguette/mpconfigboard.h
@@ -1,13 +1,11 @@
-// This configuration is for MoaT code on a BaguetteS3 prototyping board.
-
-#define MICROPY_HW_BOARD_NAME               "MoaT BaguetteS3"
+#ifndef MICROPY_HW_BOARD_NAME
+// Can be set by mpconfigboard.cmake.
+#define MICROPY_HW_BOARD_NAME               "MoaT Baguette"
+#endif
 #define MICROPY_HW_MCU_NAME                 "ESP32S3"
-
-// not needed
-#define MICROPY_PY_MACHINE_I2S              (0)
 
 // Enable UART REPL for modules that have an external USB-UART and don't use native USB.
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
 
-// want BT here
-#define MICROPY_PY_BLUETOOTH (1)
+#define MICROPY_HW_I2C0_SCL                 (48)
+#define MICROPY_HW_I2C0_SDA                 (47)

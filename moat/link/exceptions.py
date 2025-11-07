f638389a557ac31862063fa7576843ef069b2832
Matthias Urlichs <matthias@urlichs.de>
2025-11-07 09:50:34 +0100

    Proxy-export our bunch of exceptions

--- moat/link/exceptions.py
+++ moat/link/exceptions.py
@@ -6,6 +6,8 @@ Currently this is (mostly) a re-import of moat.kv.exceptions.
 
 from __future__ import annotations
 
+from moat.lib.codec.proxy import as_proxy
+
 
 class MoaTLinkError(RuntimeError):
     """Superclass of all MoaT-KV errors.
@@ -16,29 +18,36 @@ class MoaTLinkError(RuntimeError):
     pass
 
 
+@as_proxy("_EMLCl")
 class ClientError(MoaTLinkError):  # noqa: D101
     pass
 
 
+@as_proxy("_EMLClC")
 class ClientCancelledError(ClientError):  # noqa: D101
     pass
 
 
+@as_proxy("_EMLClA")
 class AuthError(MoaTLinkError):  # noqa: D101
     pass
 
 
+@as_proxy("_EMLClDt")
 class OutOfDateError(MoaTLinkError):  # noqa: D101
     pass
 
 
+@as_proxy("_EMLClSup")
 class ServiceSupplanted(MoaTLinkError):  # noqa: D101
     pass
 
 
+@as_proxy("_EMLClSnF")
 class ServiceNotFound(MoaTLinkError):  # noqa: D101
     pass
 
 
+@as_proxy("_EMLClSnS")
 class ServiceNotStarted(UserWarning):  # noqa: D101
     pass

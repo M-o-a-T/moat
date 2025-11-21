781bab3f5fc68b1603941e956780cac6e9917170
Matthias Urlichs <matthias@urlichs.de>
2025-11-21 16:47:44 +0100

    Make tests work when not root
    or not on a kernel that has the gpio-mockup module

--- tests/moat_lib_gpio/test_basic.py
+++ tests/moat_lib_gpio/test_basic.py
@@ -4,27 +4,34 @@ Empty test file
 
 from __future__ import annotations
 
-from moat.lib.gpio import open_chip, Direction
-from moat.lib.gpio.test import GpioWatcher
 import anyio
+import os
 import pytest
+
+from moat.lib.gpio import open_chip
+from moat.lib.gpio.test import GpioWatcher
+from moat.util.exec import CalledProcessError
 from moat.util.exec import run as run_
 
+
 async def preload():
+    "Try to load the mock gpio kernel module"
     await run_("tests/moat_lib_gpio/test.sh")
 
-anyio.run(preload)
+
+try:
+    anyio.run(preload)
+except CalledProcessError:
+    pytest.skip("Could not set up mock GPIO module", allow_module_level=True)
 
 
 @pytest.mark.anyio
+@pytest.mark.skipif(os.geteuid() != 0, reason="needs root")
 async def test_mockup():
     """
     Empty test
     """
-    async with (
-        anyio.create_task_group() as tg,
-        GpioWatcher(interval=.05).run() as w,
-    ):
+    async with GpioWatcher(interval=0.05).run() as w:
         try:
             with open_chip(label="gpio-mockup-A") as c:
                 assert c.num_lines == 8, f"Should have eight lines, not {c.num_lines}"
@@ -36,23 +43,23 @@ async def test_mockup():
             assert c.label == "gpio-mockup-A"
 
             with c.line(1).open() as li:
-                assert li.offset==1
+                assert li.offset == 1
                 assert not li.direction
 
-                p = w.pin(c.name,1)
+                p = w.pin(c.name, 1)
 
                 p.set(False)
                 assert not li.value
 
                 p.set(True)
                 assert li.value
-                
+
                 p.set(False)
                 assert not li.value
 
             with c.line(2).open(direction=True) as li:
                 assert li.direction
-                p = w.pin(c.name,2)
+                p = w.pin(c.name, 2)
 
                 li.value = False
                 await anyio.sleep(0.1)
@@ -66,7 +73,9 @@ async def test_mockup():
                 await anyio.sleep(0.1)
                 assert p.value is False
 
+
 @pytest.mark.anyio
+@pytest.mark.skipif(os.geteuid() != 0, reason="needs root")
 async def test_poll():
     """
     Empty test
@@ -79,33 +88,33 @@ async def test_poll():
         pytest.skip("GPIO Mockup module not loaded!")
 
     async with (
-        GpioWatcher(interval=.05).run() as w,
+        GpioWatcher(interval=0.05).run() as w,
         open_chip(label="gpio-mockup-A") as c,
-        ):
+    ):
         assert c.label == "gpio-mockup-A"
 
         with c.line(3).monitor() as li:
-            p = w.pin(c.name,3)
+            p = w.pin(c.name, 3)
 
-            ali=aiter(li)
+            ali = aiter(li)
             p.set(False)
-            with anyio.move_on_after(.1):
-                s=await anext(ali)
+            with anyio.move_on_after(0.1):
+                s = await anext(ali)
                 assert not s.value
             p.set(True)
-            with anyio.fail_after(.1):
-                s=await anext(ali)
+            with anyio.fail_after(0.1):
+                s = await anext(ali)
                 assert s.value
-            with anyio.move_on_after(.1):
-                s=await anext(ali)
+            with anyio.move_on_after(0.1):
+                s = await anext(ali)
                 raise RuntimeError("two events", s)
             await anyio.sleep(0.1)
             p.set(False)
-            with anyio.fail_after(.1):
-                s=await anext(ali)
+            with anyio.fail_after(0.1):
+                s = await anext(ali)
                 assert not s.value
-            with anyio.move_on_after(.1):
-                s=await anext(ali)
+            with anyio.move_on_after(0.1):
+                s = await anext(ali)
                 raise RuntimeError("two events", s)
 
 

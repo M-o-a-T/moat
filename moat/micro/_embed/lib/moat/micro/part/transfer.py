3748481ea516201c340ed76b7c3d5e03f74b57ba
Matthias Urlichs <matthias@urlichs.de>
2025-11-26 19:19:02 +0100

    Fix the Transfer app

--- moat/micro/_embed/lib/moat/micro/part/transfer.py
+++ moat/micro/_embed/lib/moat/micro/part/transfer.py
@@ -6,16 +6,16 @@ This is intended for regularly moving data from A to B.
 
 from __future__ import annotations
 
-from moat.util import NotGiven, Path, Queue
+from moat.util import Path, Queue, QueueFull, combine_dict
 from moat.micro.cmd.base import BaseCmd
-from moat.util.compat import TaskGroup, idle, sleep
+from moat.util.compat import Event, TaskGroup, every, idle, is_async, log
 
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from moat.lib.cmd.msg import Msg
 
-    from collections.abc import Awaitable
+    from collections.abc import Awaitable, Mapping, Sequence
 
 
 class _Send:
@@ -34,49 +34,64 @@ class _Send:
 
 
 class _Step:
-    # One transfer step.
-    # Four cases:
-    # - si=so=False:
-    #   simple-call the path with incoming data
-    #   the result is the data coming back.
-    # - si=True
-    #   Open a stream to send incoming data.
-    #   A timer calls `cmd`, the result
-    # - so=True
-    #   Open a stream to receive data.
-    #   Input is sent to `cmd`, or lost. No timer.
-    # - si=so=True
-    #   Input is sent to the stream and output is received from it.
-    #   A timer may periodically run `cmd`.
+    """
+    One transfer step.
+
+    A step has an input (it's called, async) and an output (its queue).
+
+    Four cases:
+    - si=so=False:
+      simple-call the path with incoming data
+      the result is the data coming back.
+    - si=True
+      Open a stream to send incoming data.
+      A timer calls `cmd`, the result
+    - so=True
+      Open a stream to receive data.
+      Input is sent to `cmd`, or lost. No timer.
+    - si=so=True
+      Input is sent to the stream and output is received from it.
+      A timer may periodically run `cmd`.
+    """
 
     call = None  # subcmd or feeder
 
-    p = None
-    pcmd = None
+    p: Awaitable = None
     a: tuple
     k: dict
-    app: bool = False
+    append: bool = False
 
     t: int | None = None
     si: bool = False
     so: bool = False
     msg: Msg = None
 
+    last_a: Sequence | None = None
+    last_kw: Mapping | None = None
+    _ready: Event
+
     def __init__(self, t: Transfer, id: int, cfg: Path | dict):
         self.trans = t
         self.id = id
         self.q = set()  # output queue(s)
+        self.a = cfg.get("a", ())
+        self.kw = cfg.get("k", {})
+        self._ready = Event()
+        self.is_ready = self._ready.wait
+
         if isinstance(cfg, Path):
-            self.p = cfg
+            p = cfg
         else:
-            self.p = cfg.get("p", None)
-            self.pcmd = cfg.get("cmd", None)
+            p = cfg.get("p", None)
             self.si = cfg.get("si", self.si)
             self.so = cfg.get("so", self.so)
-            self.app = cfg.get("app", self.app)
-            self.t = cfg.get("t", self.t)
+            self.append = cfg.get("append", self.append)
+        if p is not None:
+            self.p = t.root.sub_at(p)
 
-    async def task(self):
+    async def run(self) -> None:
+        if self.p is not None:
+            await self.p.rdy_()
         if self.so:
             await self.run_o()
         elif self.si:
@@ -84,47 +99,72 @@ class _Step:
         else:
             await self.run_t()
 
-    async def run_i(self):
+    async def run_i(self) -> None:
         "Open a write-only pipe to the remote"
-        async with self.root.stream_out(self.p, *self.a, **self.k) as self.msg:
+        async with self.p.stream_out(self.p, *self.a, **self.kw) as self.msg:
+            self._ready.set()
             await self.run_t()
 
-    async def run_o(self):
+    async def run_o(self) -> None:
         "Open a pipe to the remote and read from it"
-        async with (self.root.stream if self.si else self.root.stream_in)(
-            self.p, *self.a, **self.k
+        async with (self.p.stream if self.si else self.p.stream_in)(
+            self.p, *self.a, **self.kw
         ) as msg:
+            self._ready.set()
+            if self.si:
+                self.msg = msg
             async for m in msg:
-                await self.cont(self.id, m.a, m.kw)
+                await self.cont(m.a, m.kw)
 
-    async def run_t(self):
-        "Run the timer."
-        if not self.t:
-            return await idle()
-        while True:
-            if self.p is None:
-                await self.cont(self.id, self.a, self.kw)
-            else:
-                msg = await self.root.cmd(self.p, *self.a, **self.kw)
-                if not self.so:
-                    await self.cont(self.id, msg.a, msg.kw)
-            await sleep(self.t)
+    async def run_t(self) -> None:
+        "Run the task."
+        self._ready.set()
+        await idle()
 
-    async def __call__(self, a: list, kw: dict) -> tuple[list, dict] | None:
+    async def __call__(self, akw: tuple[list, dict] | None = None) -> tuple[list, dict] | None:
+        """
+        The previous step delivers data to us.
+        """
+        if akw is None:
+            a = ()
+            kw = {}
+        else:
+            a, kw = akw
         if self.si:
             await self.msg.send(*a, **kw)
             return
 
-        if self.p is not None:
-            msg = await self.root.cmd(self.p, *a, **kw)
+        if self.so:
+            # nothing to do
+            log(f"Dropped {a} {kw}")
+            pass
+
+        elif self.p is not None:
+            if self.a:
+                if self.append:
+                    a = self.a + a
+                else:
+                    a = a + self.a
+            if self.kw:
+                kw = combine_dict(kw, self.kw)
+            msg = await self.p.cmd((), *a, **kw)
             if not self.so:
-                await self.cont(self.id, msg.a, msg.kw)
-        elif not self.so:
-            await self.cont(self.id, a, kw)
+                await self.cont(msg.args, msg.kw)
+        else:
+            # simply pass on
+            await self.cont(a, kw)
 
     async def cont(self, a, kw) -> Awaitable:
+        """
+        Take this data and continue to the next step
+        """
         for q in self.q:
-            await q((a, kw))
+            try:
+                res = q((a, kw))
+                if is_async(res):
+                    await res
+            except QueueFull:
+                pass
 
 
 class Transfer(BaseCmd):
@@ -136,14 +176,20 @@ class Transfer(BaseCmd):
     - s: an array of calls.
          Either a path, or a map with
       - p: the path to use
-      - cmd: auxiliary command for asymmetric streams
       - a: positional arguments
       - k: keyword arguments
+      - append: flag to add values at the end of a instead of in front
       - si: Stream-in; bool, defaults to False.
       - so: Stream-out; bool, defaults to False.
-      - app: Flag whether to append incoming positional arguments.
-             If False (the default), they're prepended.
+
+      TODO: we might want a step for argument mangling
+
+      - put: path (or list of paths) to insert data into the arguments
+      - get: path (or list of paths) to extract data from the result
     """
+
+    tg: TaskGroup
+    steps: list[_Step]
 
     def __init__(self, cfg):
         super().__init__(cfg)
@@ -152,14 +198,25 @@ class Transfer(BaseCmd):
 
     async def task(self):
         "Step tasks runner"
-        async with TaskGroup() as tg:
-            for i, step in enumerate(self.cfg.s):
-                s = _Step(i, step)
+        async with TaskGroup() as self.tg:
+            for i, step in enumerate(self.cfg["s"]):
+                s = _Step(self, i, step)
+                if self.steps:
+                    self.steps[-1].q.add(s)
                 self.steps.append(s)
-                self.data.append(NotGiven)
 
             for s in self.steps[::-1]:
-                tg.start_soon(s.run)
+                self.tg.start_soon(s.run)
+            for s in self.steps:
+                await s.is_ready()
+            self.set_ready()
+
+            t = self.cfg.get("t", None)
+            if t is None:
+                await idle()
+            else:
+                async for _ in every(t):
+                    await self.steps[0]()
 
     async def cont(self, i, a, kw):
         "Data forwarding"
@@ -170,7 +227,7 @@ class Transfer(BaseCmd):
             if not s.si:
                 await s(a, kw)
 
-    doc_w = dict(_d="data", qs="int:position (default zero)")
+    doc_w = dict(_d="data", qs="int:step (default first)")
 
     async def stream_w(self, msg: Msg):
         "send data to (first) step"
@@ -183,7 +240,9 @@ class Transfer(BaseCmd):
         else:
             await s0(msg.a, kw)
 
-    doc_r = dict(_d="data monitor", qs="int:position (default last)")
+    doc_r = dict(
+        _d="data monitor", qs="int:step (default last)", cur="bool:get current value if present"
+    )
 
     async def stream_r(self, msg: Msg):
         "read data from (last) step"
@@ -193,11 +252,14 @@ class Transfer(BaseCmd):
         s0.q.add(qp)
         try:
             if msg.can_stream():
-                async with msg.stream_out() as md:
+                async with msg.stream_out(*(s0.last_a or ()), **(s0.last_kw or {})) as md:
                     async for a, kw in q:
                         await md.send(*a, **kw)
             else:
-                a, kw = await q.get()
+                if s0.last_a is not None and msg.get("cur", False):
+                    a, kw = s0.last_a, s0.last_kw
+                else:
+                    a, kw = await q.get()
                 await md.result(*a, **kw)
         finally:
             s0.q.remove(qp)
---
+++ tests/moat_micro/test_transfer.py
@@ -0,0 +1,102 @@
+"""
+Test our ping thing
+"""
+
+from __future__ import annotations
+
+import anyio
+import pytest
+
+from moat.util import P, yload
+from moat.micro._test import mpy_stack
+
+pytestmark = pytest.mark.anyio
+
+CFG = """
+apps:
+  r: _test.MpyCmd
+  t: part.Transfer
+  _sys: _sys.Cmd
+  b: _test.Cmd
+r:
+  mplex: true
+  cfg:
+    apps:
+      r: stdio.StdIO
+      c: cfg.Cmd
+
+      a: _test.Cmd
+      b: _test.Cmd
+      t: part.Transfer
+
+      _sys: _sys.Cmd
+    r:
+      link: &link
+        lossy: false
+        guarded: false
+      log:
+        txt: "S"
+    t:
+      t: 0.1
+      s:
+      - p: !P a.nit
+      # - p: !P a.echo
+      - p: !P b.store
+
+  link: *link
+  log:
+    txt: "M"
+
+t:
+  t: 0.1
+  s:
+  - p: !P r.a.nit
+  # - p: !P r.a.echo
+  - p: !P b.store
+"""
+
+
+async def test_store(tmp_path):
+    "test basic store/retrieve"
+    cfg = yload(CFG, attr=True)
+    del cfg.apps.t
+    del cfg.r.cfg.apps.t
+
+    async with (
+        mpy_stack(tmp_path, cfg) as d,
+        d.sub_at(P("r.a")) as xa,
+    ):
+        await xa.store(1, 2, 3)
+        await xa.store(4)
+        res = await xa.store()
+        assert res == [1, 2, 3, 4]
+
+
+async def test_transfer_here(tmp_path):
+    "test data foo"
+    cfg = yload(CFG, attr=True)
+    del cfg.r.cfg.apps.t
+
+    async with (
+        mpy_stack(tmp_path, cfg, run=True) as d,
+        d.sub_at(P("b")) as xb,
+    ):
+        await anyio.sleep(1)
+        res = await xb.store()
+        assert 8 <= len(res) <= 11
+        assert list(res) == list(range(1, len(res) + 1))
+
+
+async def test_transfer_there(tmp_path):
+    "test data foo"
+    cfg = yload(CFG, attr=True)
+    del cfg.apps.t
+
+    async with (
+        mpy_stack(tmp_path, cfg) as d,
+        d.sub_at(P("r.b")) as xb,
+    ):
+        await anyio.sleep(1)
+        res = await xb.store()
+        assert 9 <= len(res) <= 14
+        assert res == list(range(1, len(res) + 1))

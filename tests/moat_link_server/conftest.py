00831d3be27b7aad1a70364354eba62d81ab5712
Matthias Urlichs <matthias@urlichs.de>
2025-12-08 12:39:46 +0100

    Drop sub-backend-enforcers

--- tests/moat_link_server/conftest.py
+++ tests/moat_link_server/conftest.py
@@ -8,8 +8,3 @@ def reset_client_nr():  # noqa: D103
     from moat.link.server import _server as _s  # noqa: PLC0415
 
     _s._client_nr = 0  # noqa: SLF001
-
-
-@pytest.fixture
-def anyio_backend():  # noqa: D103
-    return "trio"

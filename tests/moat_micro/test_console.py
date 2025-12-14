96a9aaefab4b88af0c63e8cd9855aef7f32a15c8
Matthias Urlichs <matthias@urlichs.de>
2025-12-14 16:32:00 +0100

    Use moat.util.liner.Liner.

--- tests/moat_micro/test_console.py
+++ tests/moat_micro/test_console.py
@@ -6,9 +6,11 @@ from __future__ import annotations
 
 import anyio
 import pytest
+import sys
 
 from moat.util import P, attrdict, yload
 from moat.micro._test import mpy_stack
+from moat.util.liner import Liner
 
 pytestmark = pytest.mark.anyio
 
@@ -30,60 +32,38 @@ s:
     txt: "M"
 """
 
+# TODO add a test that uses stdio with framing plus console=True, reads
+# from the console stream and prints+verifies that via some _sys.stdout call
 
-async def test_console(tmp_path, free_tcp_port):
-    "basic console test"
+
+async def test_repl(tmp_path, free_tcp_port):
+    "basic REPL test"
     cfg = yload(CFG1, attr=True)
     cfg.s.cfg.r = attrdict(host="127.0.0.1", port=free_tcp_port, wait=False)
     cfg.r = attrdict(host="127.0.0.1", port=free_tcp_port, wait=False)
 
     async def readcons(s, con, cob=None):
-        buf = b""
-        async with anyio.create_task_group() as tg:
-            evt = anyio.Event()
+        if cob is None:
+            wr = sys.stdout.write
+        else:
 
-            @tg.start_soon
-            async def flush():
-                nonlocal evt, buf
-                while True:
-                    with anyio.move_on_after(0.1):
-                        await evt.wait()
-                        evt = anyio.Event()
-                        continue
-                    # Event did not trigger
-                    if buf:
-                        pr = buf.decode("utf-8")
-                        print(s, pr, "…")
-                        if cob is not None:
-                            cob.append(pr)
-                        buf = b""
-                    # now wait for the next incomplete line
-                    await evt.wait()
-                    evt = anyio.Event()
+            def wr(s):
+                cob.append(s)
+                sys.stdout.write(s)
 
+        async with Liner(prefix=s, writer=wr) as line:
             while True:
                 nbuf = await con(100)
                 if isinstance(nbuf, memoryview):
                     nbuf = bytes(nbuf)
-                buf += nbuf
-                idx = buf.rfind(b"\n")
-                if idx != -1:
-                    pr = buf[:idx]
-                    buf = buf[idx + 1 :]
-                    pr = pr.decode("utf-8")
-                    pr = pr.replace("\n", f"\n{s} ")
-                    print(s, pr)
-                    if cob is not None:
-                        cob.append(pr)
-                if buf:
-                    evt.set()
+                line(nbuf)
 
     async with mpy_stack(tmp_path, cfg) as d:
-        d.tg.start_soon(readcons, "CONS", d.sub_at(P("s.rd")))
+        d.tg.start_soon(readcons, "CONS ", d.sub_at(P("s.rd")))
         await d.cmd(P("r.rdy_"))
         co = d.sub_at(P("r.co"))
         cob = []
-        d.tg.start_soon(readcons, "CO", co.r, cob)
+        d.tg.start_soon(readcons, "CO ", co.r, cob)
         await co.w(b"'Foo',2*21\n")
         await anyio.sleep(0.1)
         cb = "".join(cob)

6823443348a3d437b6298caba77376da999ca278
Matthias Urlichs <matthias@urlichs.de>
2026-01-28 17:09:22 +0100

    fix readiness check

--- moat/micro/app/link.py
+++ moat/micro/app/link.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 from moat.lib.config import CFG
 from moat.lib.micro import AC_use
 from moat.lib.path import Path
-from moat.lib.rpc import BaseCmd
+from moat.lib.rpc import BaseCmd, NotReadyError
 from moat.link.announce import announcing
 from moat.link.client import Link
 from moat.util.exc import ExpKeyError
@@ -92,8 +92,13 @@ class Cmd(BaseCmd):
 
     async def handle(self, msg: Msg, rcmd: list[PathElem], *prefix: list[str]):
         "forward, possibly"
-        if self.link is None:
-            raise RuntimeError("Not ready")  # XXX maybe just return
+        if rcmd[0] == "rdy_":
+            if await self.wait_ready(wait=msg.get("wait", True)):
+                raise NotReadyError(msg.cmd, rcmd)
+
+        elif await self.wait_ready():
+            raise RuntimeError("Not ready")
+
         if self.rlink is None:
             try:
                 rpath = self.cfg["rlink"]

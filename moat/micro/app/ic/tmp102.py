71f179c986441bbc9faafdd6db1905ec1ddf541d
Matthias Urlichs <matthias@urlichs.de>
2026-02-02 11:20:37 +0100

    Consolidated sensor retrying

--- moat/micro/app/ic/tmp102.py
+++ moat/micro/app/ic/tmp102.py
@@ -1,23 +1,18 @@
 """
-TMP102 i²c
+TMP102 i²c sensor
 """
 
 from __future__ import annotations
 
 import struct
 
-from moat.lib.micro import Lock, sleep_ms
-from moat.lib.rpc import BaseCmd
-
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from moat.lib.rpc import Msg
+from moat.lib.micro import sleep_ms
+from moat.micro.util import Sensor
 
 __all__ = ["Cmd"]
 
 
-class Cmd(BaseCmd):
+class Cmd(Sensor):
     """
     This is the front-end for a TMP102 i²c temperature sensor.
 
@@ -26,14 +21,10 @@ class Cmd(BaseCmd):
         adr: Bus address to access
     """
 
-    lock: Lock
-
-    doc = dict(_c=dict(_d="TMP102 temp sensor", bus="path:i2c bus", adr="int:bus address"))
-
-    async def setup(self):
-        "Allocate lock"
-        await super().setup()
-        self.lock = Lock()
+    doc = Sensor.doc.copy()
+    doc["_c"] = doc["_c"].copy()
+    doc["_c"]["bus"] = "path:i2c bus"
+    doc["_c"]["adr"] = "path:bus address"
 
     async def task(self):
         "wrapper, for i2c bus access"
@@ -41,48 +32,21 @@ class Cmd(BaseCmd):
         async with self.root.sub_at(self.cfg["bus"]) as self.bus:
             await super().task()
 
-    doc_r = dict(
-        _d="read",
-        t="int:ms between reads when streaming, default 10s",
-        o="bool:old: wait until value differs",
-    )
+    async def read(self):
+        "read sensor"
+        await self.bus.wr(self.adr, bytes((1, 0x81, 0x10)))
+        # OneShot+Shutdown, Extended
+        for _ in range(20):
+            await sleep_ms(3)
+            res = await self.bus.wrrd(self.adr, bytes((1,)), 2)
+            if res[0] & 0x80:
+                break
+        else:
+            raise RuntimeError("No conv")
+        res = await self.bus.wrrd(self.adr, bytes((0,)), 2)
 
-    async def _rd(self):
-        async with self.lock:
-            await self.bus.wr(self.adr, bytes((1, 0x81, 0x10)))
-            # OneShot+Shutdown, Extended
-            for _ in range(20):
-                await sleep_ms(3)
-                res = await self.bus.wrrd(self.adr, bytes((1,)), 2)
-                if res[0] & 0x80:
-                    break
-            else:
-                raise RuntimeError("No conv")
-            res = await self.bus.wrrd(self.adr, bytes((0,)), 2)
         (t,) = struct.unpack(">h", res)
-        assert t & 1, (res, t)
+        if not (t & 1):
+            raise ValueError(t)
         t -= 1
-        return t
-
-    async def stream_r(self, msg: Msg):
-        "Wait for change if @o (old value) is not None"
-        t = msg.get("t", 10000)
-        o = msg.get("o", None)
-        if o is not None:
-            o = int(o * 128)
-        d = int(msg.get("d", 0) * 128)
-        if msg.can_stream:
-            async with msg.stream_out() as m:
-                while True:
-                    val = await self._rd()
-                    if o is None or abs(val - o) > d:
-                        await m.send(val / 128)
-                        if o is not None:
-                            o = val
-                    await sleep_ms(t)
-
-        val = await self._rd()
-        while o is not None and abs(val - o) <= d:
-            await sleep_ms(t)
-            val = await self._rd()
-        await msg.result(val / 128)
+        return t / 128
--- moat/micro/util.py
+++ moat/micro/util.py
@@ -6,17 +6,22 @@ from __future__ import annotations
 
 import hashlib
 
+from moat.lib.micro import Event, sleep_ms
+from moat.lib.rpc import BaseCmd
+
 from ._util import del_p, get_p, set_p
 from .files import APath, copytree
 
-# Typing
-
-from typing import TYPE_CHECKING  # isort:skip
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from moat.lib.rpc import Msg
     from moat.micro.files import MoatPath
 
-__all__ = ["del_p", "get_p", "hash256", "run_update", "set_p"]
+    from collections.abc import Awaitable, Callable
+    from typing import ClassVar
+
+__all__ = ["Repeater", "Sensor", "del_p", "get_p", "hash256", "run_update", "set_p"]
 
 
 def hash256(data):
@@ -79,3 +84,164 @@ async def _run_update(src, dest: MoatPath, check=None, cross=None, hash_fn=None)
         return res == hash256(rs)[: len(res)]
 
     await copytree(src, dest, check=check, drop=drop, cross=cross)
+
+
+class Repeater:
+    """
+    Support repeating a measurement.
+
+    Parameters:
+        min(float): Minimum value.
+        max(float): Maximum value.
+        retry(int): #Retries before erroring. Zero=infinite.
+        timer(int): Time between retries (ms)
+
+    """
+
+    val: float | None = None
+    evt: Event | bool = False
+    err: float | None = None
+    exc: Exception | None = None
+    rr: int = 0
+    retries: int = 0
+
+    def __init__(
+        self,
+        cfg: dict,
+        rdr: Callable[Awaitable[float], []],
+        min: float = -99999,  # noqa:A002
+        max: float = 99999,  # noqa:A002
+    ) -> float:
+        self.cfg = cfg
+        self.rdr = rdr
+        self.min = min
+        self.max = max
+
+    def state(self) -> dict:
+        "State monitoring stuff"
+        res = dict(r=self.retries, rr=self.rr, val=self.val)
+        if self.err is not None:
+            res["err"] = self.err
+            self.err = None
+        return res
+
+    async def get(self):
+        "Read the next bit"
+
+        # This dance ensures that an event is only allocated when more than
+        # one task tries to read at the same time
+
+        if self.evt is True:
+            self.evt = Event()
+        if isinstance(self.evt, Event):
+            while isinstance(self.evt, Event):
+                await self.evt.wait()
+            return self.val
+
+        self.evt = True
+        self.rr = 0
+        rep = self.cfg.get("repeat", 3)
+        while True:
+            try:
+                val = await self.rdr()
+            except Exception as exc:
+                if self.exc is None:
+                    self.exc = exc
+            if self.cfg.get("min", self.min) < val < self.cfg.get("max", self.max):
+                break
+            self.err = val
+            if rep:
+                rep -= 1
+                if rep == 0:
+                    exc, self.exc = self.exc, None
+                    raise exc
+            self.rr += 1
+            self.retries += 1
+            await sleep_ms(self.cfg.get("timer", 20))
+        self.retries -= 1
+        self.exc = None
+
+        if isinstance(self.evt, Event):
+            self.evt.set()
+        self.val = val
+        return val
+
+
+class Sensor(BaseCmd):
+    """
+    This is the base class for a simple (one-value) sensor.
+
+    Parameters:
+        min(float): Minimum value.
+        max(float): Maximum value.
+        retry(int): #Retries before erroring. Zero=infinite.
+        timer(int): Time between retries (ms)
+
+    You might want to override:
+    - MIN: default minimum value
+    - MAX: default maximum value
+    """
+
+    _wants: Event
+    _gets: Event
+    val: float
+    retries: int = 0
+
+    MIN: ClassVar[int | float] = -999999
+    MAX: ClassVar[int | float] = 999999
+
+    async def setup(self):
+        "Allocate events"
+        await super().setup()
+        self._rep = Repeater(self.cfg, self.read, min=self.MIN, max=self.MAX)
+
+        self._wants = Event()
+        self._gets = Event()
+
+    doc = dict(
+        _c=dict(
+            _d="Sensor base",
+            min="float:min value",
+            max="float:max value",
+            retry="int:before error; zero=inf",
+            timer="int:error delay(ms)",
+        ),
+        _d="read",
+        t="int:ms between reads when streaming, default 10s",
+        o="bool:old: wait until value differs",
+    )
+
+    async def read(self):
+        """
+        Read a value. Must be overridden.
+        """
+        raise NotImplementedError
+
+    doc_st = dict(
+        _d="Status", _r=dict(val="float:last value", r="int:Retries", rr="int:current retries")
+    )
+
+    async def cmd_st(self):
+        "Status"
+        return self._rep.state()
+
+    async def stream(self, msg: Msg):
+        "Wait for change if @o (old value) is not None"
+        t = msg.get("t", 10000)
+        o = msg.get("o", None)
+        d = msg.get("d", 0)
+        if msg.can_stream:
+            async with msg.stream_out() as m:
+                while True:
+                    val = self._rep.get()
+                    if o is None or abs(val - o) > d:
+                        await m.send(val)
+                        if o is not None:
+                            o = val
+                    await sleep_ms(t)
+
+        val = await self._rep.get()
+        while o is not None and abs(val - o) <= d:
+            await sleep_ms(t)
+            val = await self._get()
+        await msg.result(val)

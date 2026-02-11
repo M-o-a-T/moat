56b1b39fa4dc0753e65586e5b99e759740656ee2
Matthias Urlichs <matthias@urlichs.de>
2026-02-11 08:37:41 +0100

    moat-916: Add type annotations to inexact.py

--- moat/util/inexact.py
+++ moat/util/inexact.py
@@ -6,6 +6,11 @@ from __future__ import annotations
 
 from math import isclose
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Self
+
 __all__ = ["InexactFloat"]
 
 
@@ -28,9 +33,19 @@ class InexactFloat(float):
         for a=c.
     """
 
-    __slots__ = ("abs", "digits", "rel")
+    __slots__ = ("_abs", "_digits", "_rel")
 
-    def __new__(cls, val, rel=1e-06, abs=1e-12, digits=3):  # noqa: A002
+    _rel: float
+    _abs: float
+    _digits: int
+
+    def __new__(
+        cls,
+        val: float,
+        rel: float = 1e-06,
+        abs: float = 1e-12,  # noqa: A002
+        digits: int = 3,
+    ) -> Self:
         """
         Args:
             val: the exact value.
@@ -45,35 +60,37 @@ class InexactFloat(float):
         res._digits = digits
         return res
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         raise TypeError(f"unhashable type: {type(self)}")
 
-    def __eq__(self, b):
+    def __eq__(self, b: object) -> bool:
         """Check if the two floats are mostly-equal."""
-        if isclose(self, b, rel_tol=self._rel, abs_tol=self._abs):
+        if isinstance(b, int | float | InexactFloat) and isclose(
+            self, b, rel_tol=self._rel, abs_tol=self._abs
+        ):
             return True
         if not isinstance(b, InexactFloat):
             return False
         return isclose(b, self, rel_tol=b._rel, abs_tol=b._abs)
 
-    def __ne__(self, b):
+    def __ne__(self, b: object) -> bool:
         return not self.__eq__(b)
 
-    def __lt__(self, other):
+    def __lt__(self, other: float | InexactFloat) -> bool:
         return super().__lt__(other) and self != other
 
-    def __gt__(self, other):
+    def __gt__(self, other: float | InexactFloat) -> bool:
         return super().__gt__(other) and self != other
 
-    def __le__(self, other):
+    def __le__(self, other: float | InexactFloat) -> bool:
         return super().__le__(other) or self == other
 
-    def __ge__(self, other):
+    def __ge__(self, other: float | InexactFloat) -> bool:
         return super().__ge__(other) or self == other
 
     # --repr
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{type(self).__name__}({super().__repr__()})"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{round(self, self._digits):.{self._digits}f}"

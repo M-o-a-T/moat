57ec91bb4bb9e26a7dd5c740201ef03654d536a0
Matthias Urlichs <matthias@urlichs.de>
2026-02-14 13:58:28 +0100

    Typechecking fixups for ty 0.17

--- moat/util/part.py
+++ moat/util/part.py
@@ -20,7 +20,7 @@ def get_part(cur: Any, p: Sequence[str | int], add: bool = False) -> Any:
             cur = getattr(cur, pp)  # type: ignore[arg-type]  # pp might be int but then getattr raises
         except (TypeError, AttributeError):
             try:
-                cur = cur[pp]
+                cur = cur[pp]  # type:ignore[invalid-argument-type]
             except TypeError:
                 if pp is None:
                     raise KeyError(p, pp) from None
@@ -28,7 +28,7 @@ def get_part(cur: Any, p: Sequence[str | int], add: bool = False) -> Any:
             except KeyError:
                 if not add:
                     raise KeyError(p, pp) from None
-                cur[pp] = nc = []
+                cur[pp] = nc = []  # type:ignore[invalid-assignment]
                 cur = nc
     return cur
 

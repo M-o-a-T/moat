134d781c08ba472bd41c061c9553d1b2db3f9ce4
Matthias Urlichs <matthias@urlichs.de>
2026-02-20 15:03:53 +0100

    Change moat-link collect to merge matching branches

--- moat/link/node/__init__.py
+++ moat/link/node/__init__.py
@@ -12,7 +12,6 @@ from moat.util import (
     NotGiven,
     attrdict,
     combine_dict,
-    to_attrdict,
 )
 from moat.lib.path import (
     Path,
@@ -339,23 +338,23 @@ class Node:
 
     def collect(self, path: Path, keep: bool = False) -> dict:
         """
-        Collate all data from the root to this node (as far as data
-        exist) and return the combined result.
+        Collate data from all matching branches for a path.
 
-        This method is used to collect default data along a path.
+        This method combines all data nodes found in the matching
+        ``NodeFinder`` branches, with more specific matches overriding
+        less specific ones.
         """
-        if self.data_ is not NotGiven:
-            res = to_attrdict(self.data_)
-        else:
-            res = attrdict()
-        slf = self
+        nf = NodeFinder(self)
         for p in path:
             try:
-                slf = slf.get(p, create=False)
+                nf.step(p)
             except KeyError:
                 break
-            if slf.data_ is not NotGiven:
-                res = combine_dict(slf.data_, res, cls=attrdict, keep=keep)
+
+        res: dict = attrdict()
+        for node in reversed(nf.matches):
+            if node.data_ is not NotGiven:
+                res = combine_dict(node.data_, res, cls=attrdict, keep=keep)
 
         return res
 
@@ -429,7 +428,22 @@ class NodeFinder:
 
     @property
     def result(self) -> Node:
-        for node, min_more, _max_more in self.steps:
-            if min_more == 0:
-                return node
+        for node in self.matches:
+            return node
         raise KeyError("No matching wildcard state")
+
+    @property
+    def matches(self) -> tuple[Node, ...]:
+        """Return all applicable branch nodes in precedence order."""
+
+        seen: set[int] = set()
+        res: list[Node] = []
+        for node, min_more, _max_more in self.steps:
+            if min_more != 0:
+                continue
+            node_id = id(node)
+            if node_id in seen:
+                continue
+            seen.add(node_id)
+            res.append(node)
+        return tuple(res)

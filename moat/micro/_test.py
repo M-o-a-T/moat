1075af41c1a42e33383d4d42e11bdb2a44da8034
Matthias Urlichs <matthias@urlichs.de>
2026-02-25 21:04:27 +0100

    Simplify and fix syntax for µPy.

--- moat/micro/_test.py
+++ moat/micro/_test.py
@@ -90,7 +90,7 @@ class MpyBuf(ProcessBuf):
         with suppress(FileExistsError):
             (lib / "asyncio").symlink_to(aio)
 
-        libp = []
+        libp = [lib, lib2]
         for p in moat.micro.__path__:
             p = Path(p) / "_embed"  # noqa:PLW2901
             if p.exists():
@@ -100,7 +100,7 @@ class MpyBuf(ProcessBuf):
         libp.append(".frozen")
 
         self.env = {
-            "MICROPYPATH": os.pathsep.join(str(x) for x in (lib, lib2, *libp)),
+            "MICROPYPATH": os.pathsep.join(str(x) for x in libp),
         }
         self.cwd = root
 
@@ -112,8 +112,8 @@ class MpyBuf(ProcessBuf):
             with (root / "moat.lrg").open("wb") as f:
                 pass
 
-        rlink(libp[0] / "boot.py", root / "boot.py")
-        rlink(libp[0] / "main_unix.py", root / "main.py")
+        rlink(libp[2] / "boot.py", root / "boot.py")
+        rlink(libp[2] / "main_unix.py", root / "main.py")
         self.argv = [
             # "strace", "-s300", "-o/tmp/mpy.log",
             pre / "build" / ("mpy-unix" + ("-dup" if dupterm else "")) / "micropython",

b217c850dd226ac7beb923fb71663b48ebdb38a9
Matthias Urlichs <matthias@urlichs.de>
2026-02-27 07:56:21 +0100

    Fix moat-loy.6: type-check moat.lib.proxy

--- moat/lib/proxy/_proxy.py
+++ moat/lib/proxy/_proxy.py
@@ -4,11 +4,13 @@ A hacked-up copy of some parts of `moat.util`.
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast, overload
 
 if TYPE_CHECKING:
+    from types import NotImplementedType
+
     from collections.abc import Callable
-    from typing import Any, TypeVar, overload
+    from typing import Any, TypeVar
 
 __all__ = [
     "DProxy",
@@ -21,15 +23,23 @@ __all__ = [
     "obj2name",
 ]
 
-NotGiven = ...  # from moat.util import NotGiven
+NotGiven: object = ...  # from moat.util import NotGiven
 
 
-_pkey = 1
-_CProxy = {}  # name > object
-_RProxy = {}  # object > name
+_pkey: int = 1
+_CProxy: dict[str, Any] = {}  # name > object
+_RProxy: dict[int, str] = {}  # object > name
 
 
-def name2obj(name, obj=NotGiven):
+@overload
+def name2obj(name: str) -> Any: ...
+
+
+@overload
+def name2obj(name: str, obj: Any) -> None: ...
+
+
+def name2obj(name: str, obj: Any = NotGiven) -> Any | None:
     """
     Given a proxy name, return the referred object.
 
@@ -42,14 +52,14 @@ def name2obj(name, obj=NotGiven):
     return None
 
 
-def obj2name(obj):
+def obj2name(obj: object) -> str:
     """
     Given a proxied object, return the name referring to it.
     """
     return _RProxy[id(obj)]
 
 
-def get_proxy(obj):
+def get_proxy(obj: object) -> str:
     """
     Given a proxied object, return the name referring to it.
 
@@ -77,10 +87,12 @@ if TYPE_CHECKING:
     def as_proxy(name: str) -> Callable[[T], T]: ...
 
     @overload
-    def as_proxy(name: str, obj: Any, replace: bool = False) -> None: ...
+    def as_proxy(name: str, obj: T, replace: bool = False) -> T: ...
 
 
-def as_proxy(name: str, obj: Any = NotImplemented, replace: bool = False):
+def as_proxy(
+    name: str, obj: Any | NotImplementedType = NotImplemented, replace: bool = False
+) -> Any | Callable[[T], T]:
     """
     Export an object as a named proxy.
 
@@ -93,7 +105,7 @@ def as_proxy(name: str, obj: Any = NotImplemented, replace: bool = False):
     # This uses NotImplemented instead of None or Ellipsis/NotGiven because
     # those two are be legitimately proxied.
 
-    def _proxy(obj):
+    def _proxy(obj: T) -> T:
         "Export @obj as a proxy."
         if not replace and name in _CProxy and _CProxy[name] is not obj:
             raise ValueError("Proxy: " + repr(name) + " already exists")
@@ -110,7 +122,7 @@ def as_proxy(name: str, obj: Any = NotImplemented, replace: bool = False):
         return obj
 
 
-def drop_proxy(p):
+def drop_proxy(p: str | object) -> None:
     """
     After sending a proxy we keep it in memory in case the remote returns
     it, or an expression with it.
@@ -137,13 +149,13 @@ class Proxy:
     through a codec. No object data are included.
     """
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.name!r})"
 
-    def ref(self):
+    def ref(self) -> Any:
         """Dereferences the proxy"""
         return name2obj(self.name)
 
@@ -156,26 +168,26 @@ class DProxy(Proxy):
     proxy structure back (if it doesn't). The object's state is included.
     """
 
-    def __init__(self, name, a, k):
+    def __init__(self, name: str, a: list[Any], k: dict[str, Any]) -> None:
         super().__init__(name)
         self.a = a
         self.k = k
 
-    def __getitem__(self, i):
+    def __getitem__(self, i: object) -> Any:
         if i in self.k:
-            return self.k[i]
+            return self.k[cast(str, i)]
         else:
             try:
-                return self.a[i]
+                return self.a[cast(int, i)]
             except TypeError:
                 from moat.lib.micro import log  # noqa: PLC0415
 
                 log("*ERR %r", self.k)
                 raise KeyError(i) from None
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         if not isinstance(other, DProxy):
-            return NotImplemented
+            return False
 
         # Split into several lines so we can selectively set breakpoints
         # when debugging
@@ -185,13 +197,13 @@ class DProxy(Proxy):
             return False
         return True
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return (
             f"{self.__class__.__name__}({self.name!r},"
             + ",".join(repr(x) for x in (self.a, self.k))
             + ")"
         )
 
-    def ref(self):
+    def ref(self) -> Any:
         """Dereferences the proxy"""
         return name2obj(self.name)
